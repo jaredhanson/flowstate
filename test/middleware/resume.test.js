65a311d42732fe6479120f1e4fbba3f4f11089a6
Jared Hanson <jaredhanson@gmail.com>
2016-11-11 13:54:04 -0800

    More test cases.

--- test/middleware/resume.test.js
+++ test/middleware/resume.test.js
@@ -101,6 +101,85 @@ describe('middleware/resume', function() {
     });
   });
   
+  describe('resuming previous state with non-yielding state', function() {
+    var dispatcher = {
+      _resume: function(name, err, req, res, next){ next(); },
+      _transition: function(name, from, err, req, res, next){ next(); }
+    };
+    var store = {
+      load: function(){},
+      destroy: function(){}
+    };
+    
+    before(function() {
+      sinon.stub(store, 'load').yields(null);
+      sinon.stub(store, 'destroy').yields(null);
+      sinon.spy(dispatcher, '_resume');
+      sinon.spy(dispatcher, '_transition');
+    });
+    
+    after(function() {
+      dispatcher._transition.restore();
+      dispatcher._resume.restore();
+      store.destroy.restore();
+      store.load.restore();
+    });
+    
+    
+    var request, err;
+    before(function(done) {
+      chai.connect.use(resumeState(dispatcher, store, { from: 'baz' }))
+        .req(function(req) {
+          request = req;
+          request.state = { handle: '22345678', name: 'bar', y: 2, prev: '12345678' };
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should not error', function() {
+      expect(err).to.be.undefined;
+    });
+    
+    it('should preserve state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        handle: '22345678',
+        name: 'bar',
+        y: 2,
+        prev: '12345678'
+      });
+    });
+    
+    it('should not set yieldState', function() {
+      expect(request.yieldState).to.be.undefined;
+    });
+    
+    it('should not call store#destroy', function() {
+      expect(store.destroy).to.not.have.been.called;
+    });
+    
+    it('should not call store#load', function() {
+      expect(store.load).to.not.have.been.called;
+    });
+    
+    it('should call dispatcher#_transition', function() {
+      expect(dispatcher._transition).to.have.been.calledOnce;
+      var call = dispatcher._transition.getCall(0);
+      expect(call.args[0]).to.equal('bar');
+      expect(call.args[1]).to.equal('baz');
+    });
+    
+    it('should call dispatcher#_resume', function() {
+      expect(dispatcher._resume).to.have.been.calledOnce;
+      var call = dispatcher._resume.getCall(0);
+      expect(call.args[0]).to.equal('bar');
+    });
+  });
+  
   describe('resuming previous state without current state, where current state is loaded and resumes', function() {
     var dispatcher = {
       _resume: function(name, err, req, res, next){ next(); },
