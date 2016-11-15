12f5a0f598655c4f9ad3fda0157f398999df721f
Jared Hanson <jaredhanson@gmail.com>
2016-11-14 16:29:41 -0800

    Add tests.

--- test/middleware/resumeError.test.js
+++ test/middleware/resumeError.test.js
@@ -525,4 +525,84 @@ describe('middleware/resumeError', function() {
     });
   });
   
+  describe('attempting to resume previous state without current state, where current state is loaded and proceeds to default behavior', function() {
+    var dispatcher = {
+      _resume: function(name, err, req, res, next){ next(); },
+      _transition: function(name, from, err, req, res, next){ next(err); }
+    };
+    var store = {
+      load: function(){},
+      destroy: function(){}
+    };
+    
+    before(function() {
+      sinon.stub(store, 'load').yields(null, { handle: '22345678', name: 'bar', y: 2 });
+      sinon.stub(store, 'destroy').yields(null);
+      sinon.spy(dispatcher, '_resume');
+      sinon.spy(dispatcher, '_transition');
+    });
+    
+    after(function() {
+      dispatcher._transition.restore();
+      dispatcher._resume.restore();
+      store.destroy.restore();
+      store.load.restore();
+    });
+    
+    
+    var request, err;
+    before(function(done) {
+      chai.connect.use(resumeStateError(dispatcher, store, { from: 'bar' }))
+        .req(function(req) {
+          request = req;
+          req.query = { state: '22345678' };
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch(new Error('something went wrong'));
+    });
+    
+    it('should not error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something went wrong');
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        handle: '22345678',
+        name: 'bar',
+        y: 2
+      });
+    });
+    
+    it('should not set yieldState', function() {
+      expect(request.yieldState).to.be.undefined;
+    });
+    
+    it('should call store#load', function() {
+      expect(store.load).to.have.been.calledOnce;
+      var call = store.load.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('22345678');
+    });
+    
+    it('should not call store#destroy', function() {
+      expect(store.destroy).to.have.been.calledOnce;
+      var call = store.destroy.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('22345678');
+    });
+    
+    it('should not call dispatcher#_transition', function() {
+      expect(dispatcher._transition).to.not.have.been.called;
+    });
+    
+    it('should not call dispatcher#_resume', function() {
+      expect(dispatcher._resume).to.not.have.been.called;
+    });
+  });
+  
 });
