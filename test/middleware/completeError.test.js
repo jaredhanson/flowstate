e925990c7dc5adf13aa574558b72e830f5a86ceb
Samuel Judson <samuel.judson@auth0.com>
2017-10-31 15:15:30 -0500

    Add support for multiple stores.

--- test/middleware/completeError.test.js
+++ test/middleware/completeError.test.js
@@ -105,6 +105,101 @@ describe('middleware/completeError', function() {
     });
   }); // resuming previous state from current state
   
+  describe('resuming previous state from current state in `multi` mode', function() {
+    var dispatcher = {
+      _resume: function(name, err, req, res, next){ next(); },
+      _transition: function(name, from, err, req, res, next){ next(err); }
+    };
+    var fstore = {
+      destroy: function(){}
+    };
+    var tstore = {
+      load: function(){}
+    };
+    
+    before(function() {
+      sinon.stub(tstore, 'load').yields(null, { name: 'foo', x: 1 });
+      sinon.stub(fstore, 'destroy').yields(null);
+      sinon.spy(dispatcher, '_resume');
+      sinon.spy(dispatcher, '_transition');
+    });
+    
+    after(function() {
+      dispatcher._transition.restore();
+      dispatcher._resume.restore();
+      fstore.destroy.restore();
+      tstore.load.restore();
+    });
+    
+    
+    var request, err;
+    before(function(done) {
+      chai.connect.use(completeStateError(dispatcher, { fstore: fstore, tstore: tstore }, { multi: true }))
+        .req(function(req) {
+          request = req;
+          request.state = { handle: '22345678', name: 'bar', y: 2, prev: '12345678' };
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch(new Error('something went wrong'));
+    });
+    
+    it('should not error', function() {
+      expect(err).to.be.undefined;
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        name: 'foo',
+        x: 1
+      });
+    });
+    
+    it('should set yieldState', function() {
+      expect(request.yieldState).to.be.an('object');
+      expect(request.yieldState).to.deep.equal({
+        handle: '22345678',
+        name: 'bar',
+        y: 2,
+        prev: '12345678'
+      });
+    });
+    
+    it('should call fstore#destroy', function() {
+      expect(fstore.destroy).to.have.been.calledOnce;
+      var call = fstore.destroy.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('22345678');
+    });
+    
+    it('should call tstore#load', function() {
+      expect(tstore.load).to.have.been.calledOnce;
+      var call = tstore.load.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('12345678');
+    });
+    
+    it('should call dispatcher#_transition', function() {
+      expect(dispatcher._transition).to.have.been.calledOnce;
+      var call = dispatcher._transition.getCall(0);
+      expect(call.args[0]).to.equal('foo');
+      expect(call.args[1]).to.equal('bar');
+      expect(call.args[2]).to.be.an.instanceOf(Error);
+      expect(call.args[2].message).to.equal('something went wrong');
+    });
+    
+    it('should call dispatcher#_resume', function() {
+      expect(dispatcher._resume).to.have.been.calledOnce;
+      var call = dispatcher._resume.getCall(0);
+      expect(call.args[0]).to.equal('foo');
+      expect(call.args[1]).to.be.an.instanceOf(Error);
+      expect(call.args[1].message).to.equal('something went wrong');
+    });
+  }); // resuming previous state from current state in `multi` mode
+  
   describe('resuming previous state with non-yielding state', function() {
     var dispatcher = {
       _resume: function(name, err, req, res, next){ next(); },
