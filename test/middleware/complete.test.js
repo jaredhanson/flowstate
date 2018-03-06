d9d118f034a6c57b8f886f15c6fec60246551483
Jared Hanson <jaredhanson@gmail.com>
2018-03-06 09:42:00 -0800

    Add test case for loading state from body parameter.

--- test/middleware/complete.test.js
+++ test/middleware/complete.test.js
@@ -651,6 +651,111 @@ describe('middleware/complete', function() {
     });
   }); // resuming parent state from unloaded named state query parameter
   
+  describe('resuming parent state from unloaded named state body parameter', function() {
+    var dispatcher = {
+      _resume: function(name, err, req, res, next){ next(); },
+      _transition: function(name, from, err, req, res, next){ next(); }
+    };
+    var store = {
+      load: function(){},
+      destroy: function(){}
+    };
+    
+    before(function() {
+      var stub = sinon.stub(store, 'load');
+      stub.onCall(0).yields(null, { handle: '22345678', name: 'bar', y: 2, prev: '12345678' })
+      stub.onCall(1).yields(null, { name: 'foo', x: 1 });
+      
+      sinon.stub(store, 'destroy').yields(null);
+      sinon.spy(dispatcher, '_resume');
+      sinon.spy(dispatcher, '_transition');
+    });
+    
+    after(function() {
+      dispatcher._transition.restore();
+      dispatcher._resume.restore();
+      store.destroy.restore();
+      store.load.restore();
+    });
+    
+    
+    var request, err;
+    before(function(done) {
+      chai.connect.use(completeState(dispatcher, store, { name: 'bar' }))
+        .req(function(req) {
+          request = req;
+          req.body = { state: '22345678' };
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should not error', function() {
+      expect(err).to.be.undefined;
+    });
+    
+    it('should set skip error flag', function() {
+      expect(request._skipResumeError).to.equal(true);
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        name: 'foo',
+        x: 1
+      });
+    });
+    
+    it('should set yieldState', function() {
+      expect(request.yieldState).to.be.an('object');
+      expect(request.yieldState).to.deep.equal({
+        handle: '22345678',
+        name: 'bar',
+        y: 2,
+        prev: '12345678'
+      });
+    });
+    
+    it('should call store#load to load current state', function() {
+      expect(store.load).to.have.been.calledTwice;
+      var call = store.load.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('22345678');
+    });
+    
+    it('should call store#destroy to destroy current state', function() {
+      expect(store.destroy).to.have.been.calledOnce;
+      var call = store.destroy.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('22345678');
+    });
+    
+    it('should call store#load to load parent state', function() {
+      expect(store.load).to.have.been.calledTwice;
+      var call = store.load.getCall(1);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('12345678');
+    });
+    
+    it('should call dispatcher#_transition', function() {
+      expect(dispatcher._transition).to.have.been.calledOnce;
+      var call = dispatcher._transition.getCall(0);
+      expect(call.args[0]).to.equal('foo');
+      expect(call.args[1]).to.equal('bar');
+      expect(call.args[2]).to.be.null;
+    });
+    
+    it('should call dispatcher#_resume', function() {
+      expect(dispatcher._resume).to.have.been.calledOnce;
+      var call = dispatcher._resume.getCall(0);
+      expect(call.args[0]).to.equal('foo');
+      expect(call.args[1]).to.be.undefined;
+    });
+  }); // resuming parent state from unloaded named state body parameter
+  
   describe('attempting to resume parent state from state and proceeding to default behavior', function() {
     var dispatcher = {
       _resume: function(name, err, req, res, next){ next(); },
