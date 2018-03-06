51e38ded44235249f00d1fc9b3c94cdd6780bf41
Jared Hanson <jaredhanson@gmail.com>
2018-03-06 11:08:46 -0800

    Simplify error condition, add tests.

--- test/middleware/complete.test.js
+++ test/middleware/complete.test.js
@@ -1200,7 +1200,7 @@ describe('middleware/complete', function() {
     });
   }); // attempting to resume parent state from unloaded named state and proceeding to default behavior
   
-  describe('attempting to resume previous state without state', function() {
+  describe('attempting to resume parent state without state', function() {
     var dispatcher = {
       _resume: function(){},
       _transition: function(){}
@@ -1265,9 +1265,9 @@ describe('middleware/complete', function() {
     it('should not call dispatcher#_resume', function() {
       expect(dispatcher._resume).to.not.have.been.called;
     });
-  }); // attempting to resume previous state without state
+  }); // attempting to resume parent state without state
   
-  describe('attempting to resume previous state which is not found', function() {
+  describe('attempting to resume parent state which is not found', function() {
     var dispatcher = {
       _resume: function(name, err, req, res, next){ next(); },
       _transition: function(name, from, err, req, res, next){ next(); }
@@ -1348,7 +1348,81 @@ describe('middleware/complete', function() {
     it('should not call dispatcher#_resume', function() {
       expect(dispatcher._resume).to.not.have.been.called;
     });
-  }); // attempting to resume previous state which is not found
+  }); // attempting to resume parent state which is not found
+  
+  describe('attempting to resume parent state from unloaded state which is not found', function() {
+    var dispatcher = {
+      _resume: function(name, err, req, res, next){ next(); },
+      _transition: function(name, from, err, req, res, next){ next(); }
+    };
+    var store = {
+      load: function(){},
+      destroy: function(){}
+    };
+    
+    before(function() {
+      sinon.stub(store, 'load').yields(null, undefined);
+      sinon.stub(store, 'destroy').yields(null);
+      sinon.spy(dispatcher, '_resume');
+      sinon.spy(dispatcher, '_transition');
+    });
+    
+    after(function() {
+      dispatcher._transition.restore();
+      dispatcher._resume.restore();
+      store.destroy.restore();
+      store.load.restore();
+    });
+    
+    
+    var request, err;
+    before(function(done) {
+      chai.connect.use(completeState(dispatcher, store))
+        .req(function(req) {
+          request = req;
+          req.query = { state: '12345678' };
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.constructor.name).to.equal('MissingStateError');
+      expect(err.message).to.equal('Failed to load state');
+      expect(err.handle).to.equal('12345678');
+    });
+    
+    it('should set skip error flag', function() {
+      expect(request._skipResumeError).to.equal(true);
+    });
+    
+    it('should preserve state', function() {
+      expect(request.state).to.be.undefined;
+    });
+    
+    it('should not set yieldState', function() {
+      expect(request.yieldState).to.be.undefined;
+    });
+    
+    it('should not call store#destroy', function() {
+      expect(store.destroy).to.not.have.been.called;
+    });
+    
+    it('should call store#load to load parent state', function() {
+      expect(store.load).to.have.been.calledOnce;
+      var call = store.load.getCall(0);
+      expect(call.args[0]).to.equal(request);
+      expect(call.args[1]).to.equal('12345678');
+    });
+    
+    it('should not call dispatcher#_resume', function() {
+      expect(dispatcher._resume).to.not.have.been.called;
+    });
+  }); // attempting to resume parent state from unloaded state which is not found
   
   describe('encountering an error destroying state', function() {
     var dispatcher = {
