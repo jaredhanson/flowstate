14d4be1765e6fc8a63fadb609eaa63b1a535e1a6
Jared Hanson <jaredhanson@gmail.com>
2018-03-13 12:39:36 -0700

    Start adding integration tests.

---
+++ test/integration/load.test.js
@@ -0,0 +1,73 @@
+var chai = require('chai')
+  , expect = require('chai').expect
+  , sinon = require('sinon')
+  , Dispatcher = require('../../lib/manager')
+  , SessionStore = require('../../lib/stores/session')
+  , completeState = require('../../lib/middleware/complete');
+
+
+describe('Dispatcher#complete', function() {
+  
+  describe('resuming parent state from loaded, unnamed state', function() {
+    
+    var request, response, err;
+    before(function(done) {
+      var store = new SessionStore();
+      var dispatcher = new Dispatcher(store);
+      
+      dispatcher.use('foo', null, [function(req, res, next) {
+        res.end('foo.');
+      }]);
+      
+      
+      chai.connect.use(dispatcher.complete())
+        .req(function(req) {
+          request = req;
+          request.state = { handle: '22345678', name: 'bar', y: 2, prev: '12345678' };
+          request.session = { state: {} };
+          request.session.state['12345678'] = { name: 'foo', x: 1 };
+          request.session.state['22345678'] = { name: 'bar', y: 2, prev: '12345678' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        handle: '12345678',
+        name: 'foo',
+        x: 1
+      });
+    });
+    
+    it('should set yieldState', function() {
+      expect(request.yieldState).to.be.an('object');
+      expect(request.yieldState).to.deep.equal({
+        handle: '22345678',
+        name: 'bar',
+        y: 2,
+        prev: '12345678'
+      });
+    });
+    
+    it('should remove state from session', function() {
+      expect(request.session).to.deep.equal({
+        state: {
+          '12345678': {
+            name: 'foo',
+            x: 1
+          }
+        }
+      });
+    });
+    
+    it('should respond', function() {
+      expect(response._data).to.equal('foo.');
+    });
+  }); // resuming parent state from loaded, unnamed state
+  
+});
