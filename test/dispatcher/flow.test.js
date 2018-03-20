1fc0acf5c81e5b91f081d2e754a7124566f036b7
Jared Hanson <jaredhanson@gmail.com>
2018-03-20 11:29:58 -0700

    Add test case.

--- test/dispatcher/flow.test.js
+++ test/dispatcher/flow.test.js
@@ -275,6 +275,107 @@ describe('Dispatcher#flow', function() {
     });
   }); // resuming parent state after transition through synthesized state with transition from specified, loaded, named state
   
+  describe('resuming parent state after transition through synthesized state without transition from specified, loaded, named state', function() {
+    
+    var request, response, err;
+    before(function(done) {
+      var dispatcher = new Dispatcher();
+      
+      // foo
+      dispatcher.use('foo', null, [
+        function(req, res, next) {
+          res.__text += ' ' + req.state.name + '(' + req.yieldState.name + ')';
+          res.end(res.__text);
+        },
+        function(err, req, res, next) {
+          res.__text += ' E:' + req.state.name + '(' + req.yieldState.name + ')';
+          res.end(res.__text);
+        }
+      ]);
+      
+      dispatcher.transition('foo', 'bar', [
+        function(req, res, next) {
+          req.state.y = req.yieldState.y;
+          req.state.z = req.yieldState.z;
+          res.__text += ' --' + req.yieldState.name + '/' + req.state.name + '-->';
+          next();
+        },
+        function(err, req, res, next) {
+          res.__text += ' --E:' + req.yieldState.name + '/' + req.state.name + '-->';
+          next();
+        }
+      ]);
+      
+      // bar
+      dispatcher.use('bar', null, [
+        function(req, res, next) {
+          res.__text += ' ' + req.state.name + '(' + req.yieldState.name + ')';
+          res.completePrompt();
+        },
+        function(err, req, res, next) {
+          res.__text += ' E:' + req.state.name + '(' + req.yieldState.name + ')';
+          res.completePrompt(err);
+        }
+      ]);
+      
+      
+      chai.express.handler(dispatcher.flow('baz', { through: 'bar' }))
+        .req(function(req) {
+          request = req;
+          request.state = { handle: '22345678', name: 'baz', z: 3, prev: '12345678' };
+          request.session = { state: {} };
+          request.session.state['12345678'] = { name: 'foo', x: 1 };
+          request.session.state['22345678'] = { name: 'baz', z: 3, prev: '12345678' };
+        })
+        .res(function(res) {
+          res.__text = 'baz'
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .next(function(err) {
+          console.log(err);
+          done(err);
+        })
+        .dispatch();
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        handle: '12345678',
+        name: 'foo',
+        x: 1,
+        y: undefined,
+        z: undefined
+      });
+    });
+    
+    it('should set yieldState', function() {
+      expect(request.yieldState).to.be.an('object');
+      expect(request.yieldState).to.deep.equal({
+        name: 'bar',
+        prev: '12345678'
+      });
+    });
+    
+    it('should remove state from session', function() {
+      expect(request.session).to.deep.equal({
+        state: {
+          '12345678': {
+            name: 'foo',
+            x: 1
+          }
+        }
+      });
+    });
+    
+    it('should respond', function() {
+      expect(response._data).to.equal('baz bar(baz) --bar/foo--> foo(bar)');
+    });
+  }); // resuming parent state after transition through synthesized state without transition from specified, loaded, named state
+  
   // THIS IS AN ERROR SOMEWHERE
   describe.skip('resuming parent state after transition through synthesized state from loaded, named state', function() {
     
