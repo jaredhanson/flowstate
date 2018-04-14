02881ad8cd104763280722a5949d72374b112627
Jared Hanson <jaredhanson@gmail.com>
2018-04-14 09:50:17 -0700

    Add tests.

--- test/dispatcher/flow.test.js
+++ test/dispatcher/flow.test.js
@@ -993,6 +993,103 @@ describe('Dispatcher#flow', function() {
     });
   }); // rendering from a new state where parent state is carried in query param
   
+  describe('rendering from a new state where parent state is carried in body param', function() {
+    var hc = 1;
+    var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+      , request, response, layout, err;
+      
+    before(function() {
+      sinon.spy(dispatcher._store, 'load');
+      sinon.spy(dispatcher._store, 'save');
+      sinon.spy(dispatcher._store, 'update');
+      sinon.spy(dispatcher._store, 'destroy');
+    });
+      
+    before(function(done) {
+      function handler(req, res, next) {
+        res.locals.identifierHint = 'alice@example.com';
+        res.render('views/' + req.state.name);
+      }
+      
+      
+      chai.express.handler(dispatcher.flow('login', handler))
+        .req(function(req) {
+          request = req;
+          request.body = { state: 'H1' };
+          request.session = { state: {} };
+          request.session.state['H1'] = { name: 'start', foo: 'bar' };
+        })
+        .res(function(res) {
+          res.locals = {};
+        })
+        .render(function(res, lay) {
+          layout = lay;
+          res.end();
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    after(function() {
+      dispatcher._store.destroy.restore();
+      dispatcher._store.update.restore();
+      dispatcher._store.save.restore();
+      dispatcher._store.load.restore();
+    });
+    
+    
+    it('should correctly invoke state store', function() {
+      expect(dispatcher._store.load).to.have.callCount(1);
+      var call = dispatcher._store.load.getCall(0);
+      expect(call.args[1]).to.equal('H1');
+      
+      expect(dispatcher._store.save).to.have.callCount(0);
+      expect(dispatcher._store.update).to.have.callCount(0);
+      expect(dispatcher._store.destroy).to.have.callCount(0);
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        name: 'login'
+      });
+    });
+    
+    it('should set optimized parent state', function() {
+      expect(request._state).to.be.an('object');
+      expect(request._state).to.deep.equal({
+        name: 'start',
+        foo: 'bar'
+      });
+    });
+    
+    it('should not set yieldState', function() {
+      expect(request.yieldState).to.be.undefined;
+    });
+    
+    it('should maintain state in session', function() {
+      expect(request.session).to.deep.equal({
+        state: {
+          'H1': {
+            name: 'start',
+            foo: 'bar'
+          }
+        }
+      });
+    });
+    
+    it('should render layout', function() {
+      expect(layout).to.equal('views/login');
+      expect(response.locals).to.deep.equal({
+        identifierHint: 'alice@example.com',
+        state: 'H1'
+      });
+    });
+  }); // rendering from a new state where parent state is carried in body param
+  
   describe('rendering from a current state where state is carried in query param', function() {
     var hc = 1;
     var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
@@ -1091,6 +1188,105 @@ describe('Dispatcher#flow', function() {
       });
     });
   }); // rendering from a current state where state is carried in query param
+  
+  describe('rendering from a current state where state is carried in body param', function() {
+    var hc = 1;
+    var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+      , request, response, layout, err;
+      
+    before(function() {
+      sinon.spy(dispatcher._store, 'load');
+      sinon.spy(dispatcher._store, 'save');
+      sinon.spy(dispatcher._store, 'update');
+      sinon.spy(dispatcher._store, 'destroy');
+    });
+      
+    before(function(done) {
+      function handler(req, res, next) {
+        res.locals.identifierHint = 'alice@example.com';
+        res.render('views/' + req.state.name);
+      }
+      
+      
+      chai.express.handler(dispatcher.flow('login', handler))
+        .req(function(req) {
+          request = req;
+          request.body = { state: 'H2' };
+          request.session = { state: {} };
+          request.session.state['H1'] = { name: 'start', foo: 'bar' };
+          request.session.state['H2'] = { name: 'login', failureCount: 2 };
+        })
+        .res(function(res) {
+          res.locals = {};
+        })
+        .render(function(res, lay) {
+          layout = lay;
+          res.end();
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    after(function() {
+      dispatcher._store.destroy.restore();
+      dispatcher._store.update.restore();
+      dispatcher._store.save.restore();
+      dispatcher._store.load.restore();
+    });
+    
+    
+    it('should correctly invoke state store', function() {
+      expect(dispatcher._store.load).to.have.callCount(1);
+      var call = dispatcher._store.load.getCall(0);
+      expect(call.args[1]).to.equal('H2');
+      
+      expect(dispatcher._store.save).to.have.callCount(0);
+      expect(dispatcher._store.update).to.have.callCount(0);
+      expect(dispatcher._store.destroy).to.have.callCount(0);
+    });
+    
+    it('should set state', function() {
+      expect(request.state).to.be.an('object');
+      expect(request.state).to.deep.equal({
+        name: 'login',
+        failureCount: 2
+      });
+    });
+    
+    it('should not set optimized parent state', function() {
+      expect(request._state).to.be.undefined;
+    });
+    
+    it('should not set yieldState', function() {
+      expect(request.yieldState).to.be.undefined;
+    });
+    
+    it('should maintain state in session', function() {
+      expect(request.session).to.deep.equal({
+        state: {
+          'H1': {
+            name: 'start',
+            foo: 'bar'
+          },
+          'H2': {
+            name: 'login',
+            failureCount: 2
+          }
+        }
+      });
+    });
+    
+    it('should render layout', function() {
+      expect(layout).to.equal('views/login');
+      expect(response.locals).to.deep.equal({
+        identifierHint: 'alice@example.com',
+        state: 'H2'
+      });
+    });
+  }); // rendering from a current state where state is carried in body param
   
   
   
