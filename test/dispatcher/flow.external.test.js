515f43bec2a971b55fb50dc0c9f346d006aa01ed
Jared Hanson <jaredhanson@gmail.com>
2018-04-15 11:56:34 -0700

    Add failing test.

--- test/dispatcher/flow.external.test.js
+++ test/dispatcher/flow.external.test.js
@@ -887,4 +887,108 @@ describe('Dispatcher#flow (externally-initiated)', function() {
     
   }); // responding immediately
   
+  
+  describe('failure', function() {
+    
+    // FIXME: Get the dispatcher to next here
+    describe.skip('due to state not being registered', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        function handler(req, res, next) {
+          console.log('XYZ?');
+          res.prompt('consent');
+        }
+      
+        /*
+        dispatcher.use('consent', [
+          function(req, res, next) {
+            res.redirect('/from/' + req.state.name);
+          }
+        ], null);
+        */
+      
+        chai.express.handler(dispatcher.flow('start', handler, { external: true }))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'X1' };
+            request.session = {};
+          })
+          .res(function(res) {
+            response = res;
+          })
+          .next(function(e) {
+            console.log('ERROR');
+            err = e;
+            //done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should error', function() {
+        expect(err).to.be.an.instanceOf(Error);
+        expect(err.constructor.name).to.equal('Error');
+        expect(err.message).to.equal("Cannot find flow 'login'");
+      });
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(1);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'consent',
+          parent: 'H1'
+        });
+      });
+    
+      it('should set locals', function() {
+        expect(request.locals).to.deep.equal({});
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should persist state in session', function() {
+        //expect(request.session.state['H1'].initiatedAt).to.be.a('number')
+        //delete request.session.state['H1'].initiatedAt;
+      
+        expect(request.session).to.deep.equal({
+          state: {
+            'H1': {
+              name: 'start'
+            }
+          }
+        });
+      });
+    
+      it('should respond', function() {
+        expect(response.getHeader('Location')).to.equal('/from/consent?state=H1');
+      });
+    }); // due to state not being registered
+    
+  });
+  
 });
