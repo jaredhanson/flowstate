24cae072a067c4f01c5e016cd88d3986435e1cce
Jared Hanson <jaredhanson@gmail.com>
2018-04-14 18:18:35 -0700

    Clean up tests.

--- test/dispatcher/flow.external.test.js
+++ test/dispatcher/flow.external.test.js
@@ -545,6 +545,206 @@ describe('Dispatcher#flow (externally-initiated)', function() {
     
   }); // prompting via redirect
   
+  describe('prompting via render', function() {
+    
+    describe('without options', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, layout, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        function handler(req, res, next) {
+          res.prompt('consent');
+        }
+      
+        dispatcher.use('consent', [
+          function(req, res, next) {
+            res.render('views/' + req.state.name);
+          }
+        ], null);
+      
+      
+        chai.express.handler(dispatcher.flow('start', handler, { external: true }))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'X1' };
+            request.session = {};
+          })
+          .res(function(res) {
+            res.locals = {};
+          })
+          .render(function(res, lay) {
+            layout = lay;
+            res.end();
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(1);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'consent',
+          parent: 'H1'
+        });
+      });
+    
+      it('should set locals', function() {
+        expect(request.locals).to.deep.equal({});
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should persist state in session', function() {
+        //expect(request.session.state['H1'].initiatedAt).to.be.a('number')
+        //delete request.session.state['H1'].initiatedAt;
+      
+        expect(request.session).to.deep.equal({
+          state: {
+            'H1': {
+              name: 'start'
+            }
+          }
+        });
+      });
+    
+      it('should render layout', function() {
+        expect(layout).to.equal('views/consent');
+        expect(response.locals).to.deep.equal({
+          state: 'H1'
+        });
+      });
+    }); // without options
+    
+    describe('with options', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, layout, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        function handler(req, res, next) {
+          res.prompt('consent', { scope: 'test' });
+        }
+      
+        dispatcher.use('consent', [
+          function(req, res, next) {
+            res.locals.scope = req.locals.scope;
+            res.render('views/' + req.state.name);
+          }
+        ], null);
+      
+      
+        chai.express.handler(dispatcher.flow('start', handler, { external: true }))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'X1' };
+            request.session = {};
+          })
+          .res(function(res) {
+            res.locals = {};
+          })
+          .render(function(res, lay) {
+            layout = lay;
+            res.end();
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(1);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'consent',
+          parent: 'H1'
+        });
+      });
+    
+      it('should set locals', function() {
+        expect(request.locals).to.deep.equal({
+          scope: 'test'
+        });
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should persist state in session', function() {
+        //expect(request.session.state['H1'].initiatedAt).to.be.a('number')
+        //delete request.session.state['H1'].initiatedAt;
+      
+        expect(request.session).to.deep.equal({
+          state: {
+            'H1': {
+              name: 'start'
+            }
+          }
+        });
+      });
+    
+      it('should render layout', function() {
+        expect(layout).to.equal('views/consent');
+        expect(response.locals).to.deep.equal({
+          scope: 'test',
+          state: 'H1'
+        });
+      });
+    }); // with options
+    
+  }); // prompting via render
+  
   describe('responding immediately', function() {
     
     describe('by redirecting', function() {
