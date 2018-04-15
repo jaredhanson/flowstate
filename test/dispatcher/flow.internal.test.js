a55bf7eef38836067ae966133f55b92fc6dd10b4
Jared Hanson <jaredhanson@gmail.com>
2018-04-14 19:25:21 -0700

    Clarify tests.

--- test/dispatcher/flow.internal.test.js
+++ test/dispatcher/flow.internal.test.js
@@ -904,6 +904,93 @@ describe('Dispatcher#flow (internally-driven)', function() {
       });
     }); // from new state yielding to state from query param
     
+    describe('from current state carried in query param', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, layout, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        function handler(req, res, next) {
+          res.redirect('/from/' + req.state.name);
+        }
+      
+      
+        chai.express.handler(dispatcher.flow('login', handler))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'H2' };
+            request.session = { state: {} };
+            request.session.state['H1'] = { name: 'start', foo: 'bar' };
+            request.session.state['H2'] = { name: 'login', failureCount: 2 };
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(1);
+        var call = dispatcher._store.load.getCall(0);
+        expect(call.args[1]).to.equal('H2');
+      
+        expect(dispatcher._store.save).to.have.callCount(0);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'login',
+          failureCount: 2
+        });
+      });
+    
+      it('should not set optimized parent state', function() {
+        expect(request._state).to.be.undefined;
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should maintain state in session', function() {
+        expect(request.session).to.deep.equal({
+          state: {
+            'H1': {
+              name: 'start',
+              foo: 'bar'
+            },
+            'H2': {
+              name: 'login',
+              failureCount: 2
+            }
+          }
+        });
+      });
+    
+      it('should respond', function() {
+        expect(response.getHeader('Location')).to.equal('/from/login?state=H2');
+      });
+    }); // from current state carried in query param
+    
   }); // redirecting
   
 });
