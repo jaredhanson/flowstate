5df0e158f7dbafa93d473e4166517593367ea31d
Jared Hanson <jaredhanson@gmail.com>
2018-04-15 09:22:11 -0700

    Clarify tests.

--- test/dispatcher/flow.resume.test.js
+++ test/dispatcher/flow.resume.test.js
@@ -554,6 +554,114 @@ describe('Dispatcher#flow (resume)', function() {
       });
     }); // to parent state referenced by query param
     
+    describe('to parent state referenced by custom query param', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, err;
+    
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+    
+      before(function(done) {
+        dispatcher.use('start', null, [
+          function(req, res, next) {
+            res.__track += ' ' + req.state.name + '(' + req.yieldState.name + ')';
+            next();
+          },
+          function(err, req, res, next) {
+            res.__track += ' E:' + req.state.name + '(' + req.yieldState.name + ')';
+            next(err);
+          }
+        ], [
+          function(req, res, next) {
+            res.__track += '[F]';
+            res.redirect('/from/' + req.state.name);
+          },
+          function(err, req, res, next) {
+            res.__track += '[E]';
+            next(err);
+          }
+        ]);
+      
+        function handler(req, res, next) {
+          res.__track = req.state.name;
+          next();
+        }
+      
+      
+        function getHandle(req) {
+          return req.query.s;
+        }
+      
+        chai.express.handler(dispatcher.flow('consent', handler, { getHandle: getHandle }))
+          .req(function(req) {
+            request = req;
+            request.query = { s: 'H1' };
+            request.session = { state: {} };
+            request.session.state['H1'] = { name: 'start', foo: 'bar' };
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should track correctly', function() {
+        expect(response.__track).to.equal('consent start(consent)[F]');
+      });
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(1);
+        var call = dispatcher._store.load.getCall(0);
+        expect(call.args[1]).to.equal('H1');
+      
+        expect(dispatcher._store.save).to.have.callCount(0);
+        expect(dispatcher._store.update).to.have.callCount(0);
+      
+        expect(dispatcher._store.destroy).to.have.callCount(1);
+        call = dispatcher._store.destroy.getCall(0);
+        expect(call.args[1]).to.equal('H1');
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state.handle).to.be.null;
+        expect(request.state).to.deep.equal({
+          name: 'start',
+          foo: 'bar'
+        });
+      });
+    
+      it('should set yieldState', function() {
+        expect(request.yieldState).to.be.an('object');
+        expect(request.yieldState.handle).to.be.undefined;
+        expect(request.yieldState).to.deep.equal({
+          name: 'consent'
+        });
+      });
+    
+      it('should remove completed state from session', function() {
+        expect(request.session).to.deep.equal({});
+      });
+    
+      it('should respond', function() {
+        expect(response.getHeader('Location')).to.equal('/from/start');
+      });
+    }); // to parent state referenced by custom query param
+    
   }); // finish by redirecting
   
   
