cb61a8a0282e168ccf27e9776069fb73b95ff30b
Jared Hanson <jaredhanson@gmail.com>
2018-04-17 20:10:35 -0700

    Add complete state to external flows.

--- test/dispatcher/flow.external.test.js
+++ test/dispatcher/flow.external.test.js
@@ -906,6 +906,174 @@ describe('Dispatcher#flow (externally-initiated)', function() {
       });
     }); // within flow
     
+    describe('from finish', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, layout, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        dispatcher.use('start', { finish: [
+          function(req, res, next) {
+            res.render('views/finish/' + req.state.name);
+          }
+        ]});
+        
+        function handler(req, res, next) {
+          next();
+        }
+      
+      
+        chai.express.handler(dispatcher.flow('start', handler, { external: true }))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'X1' };
+            request.session = {};
+          })
+          .res(function(res) {
+            res.locals = {};
+          })
+          .render(function(res, lay) {
+            layout = lay;
+            res.end();
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(0);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'start'
+        });
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should not persist state in session', function() {
+        expect(request.session).to.deep.equal({});
+      });
+    
+      it('should render layout', function() {
+        expect(layout).to.equal('views/finish/start');
+        expect(response.locals).to.deep.equal({});
+      });
+    }); // from finish
+    
+    describe('from finish after saving state within flow', function() {
+      var hc = 1;
+      var dispatcher = new Dispatcher({ genh: function() { return 'H' + hc++; } })
+        , request, response, layout, err;
+      
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+      
+      before(function(done) {
+        dispatcher.use('start', { finish: [
+          function(req, res, next) {
+            res.render('views/finish/' + req.state.name);
+          }
+        ]});
+        
+        function handler(req, res, next) {
+          req.state.save(function(err) {
+            if (err) { return next(err); }
+            res.locals.state = req.state.handle;
+            next();
+          });
+        }
+      
+      
+        chai.express.handler(dispatcher.flow('start', handler, { external: true }))
+          .req(function(req) {
+            request = req;
+            request.query = { state: 'X1' };
+            request.session = {};
+          })
+          .res(function(res) {
+            res.locals = {};
+          })
+          .render(function(res, lay) {
+            layout = lay;
+            res.end();
+          })
+          .end(function(res) {
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+    
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+    
+    
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(1);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(1);
+        var call = dispatcher._store.destroy.getCall(0);
+        expect(call.args[1]).to.equal('H1');
+      });
+    
+      it('should set state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: 'start'
+        });
+      });
+    
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.undefined;
+      });
+    
+      it('should remove saved state from session', function() {
+        expect(request.session).to.deep.equal({});
+      });
+    
+      it('should render layout', function() {
+        expect(layout).to.equal('views/finish/start');
+        expect(response.locals).to.deep.equal({
+          state: 'H1'
+        });
+      });
+    }); // from finish after state saving state within flow
+    
   }); // rendering
   
   
