4e01dccfeb79c68570f3e7bbd9d85ed2ac2fd2d9
Jared Hanson <jaredhanson@gmail.com>
2019-12-07 09:41:48 -0800

    Add tests.

---
+++ test/integration/oauth2.test.js
@@ -0,0 +1,9 @@
+var chai = require('chai')
+  , expect = require('chai').expect
+  , sinon = require('sinon')
+  , Dispatcher = require('../../lib/manager');
+
+
+describe('integration: OAuth 2.0', function() {
+  
+});
