0d7e64216376b5992f7a2486d52b7ea6c1d5c0b0
Jared Hanson <jaredhanson@gmail.com>
2019-12-30 13:41:35 -0800

    Add tests.

--- test/integration/oauth2.test.js
+++ test/integration/oauth2.test.js
@@ -4,6 +4,71 @@ var chai = require('chai')
   , Dispatcher = require('../../lib/manager');
 
 
-describe('integration: OAuth 2.0', function() {
+describe('integration: oauth2', function() {
+  
+  describe('redirecting for login', function() {
+    var dispatcher = new Dispatcher({ genh: function() { return 'XXXXXXXX' } })
+      , request, response, err;
+  
+    before(function() {
+      sinon.spy(dispatcher._store, 'load');
+      sinon.spy(dispatcher._store, 'save');
+      sinon.spy(dispatcher._store, 'update');
+      sinon.spy(dispatcher._store, 'destroy');
+    });
+  
+    before(function(done) {
+      function handler(req, res, next) {
+        res.redirect('/login');
+      }
+    
+      chai.express.handler(dispatcher.flow(handler, { external: true }))
+        .req(function(req) {
+          request = req;
+          request.method = 'POST';
+          request.url = '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb';
+          request.query = { response_type: 'code', client_id: 's6BhdRkqt3', state: 'xyz', redirect_uri: 'https://client.example.com/cb' };
+          request.session = {};
+        })
+        .res(function(res) {
+          response = res;
+        })
+        .end(function(res) {
+          done();
+        })
+        .dispatch();
+    });
+
+    after(function() {
+      dispatcher._store.destroy.restore();
+      dispatcher._store.update.restore();
+      dispatcher._store.save.restore();
+      dispatcher._store.load.restore();
+    });
+
+
+    it('should correctly invoke state store', function() {
+      expect(dispatcher._store.load).to.have.callCount(0);
+      expect(dispatcher._store.save).to.have.callCount(1);
+      expect(dispatcher._store.update).to.have.callCount(0);
+      expect(dispatcher._store.destroy).to.have.callCount(0);
+    });
+    
+    it('should persist state in session', function() {
+      expect(request.session).to.deep.equal({
+        state: {
+          'XXXXXXXX': {
+            name: '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb',
+            returnTo: '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+          }
+        }
+      });
+    });
+
+    it('should respond', function() {
+      expect(response.statusCode).to.equal(302);
+      expect(response.getHeader('Location')).to.equal('/login?state=XXXXXXXX');
+    });
+  }); // redirecting for login
   
 });
