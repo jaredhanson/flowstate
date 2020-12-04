cd13a74973e2eea6b464bdb55f67572de4d0d8fe
Jared Hanson <jaredhanson@gmail.com>
2020-12-04 15:43:31 -0800

    Implement basic push state.

--- test/integration/parameters.test.js
+++ test/integration/parameters.test.js
@@ -101,6 +101,99 @@ describe('integration: parameters', function() {
       });
     }); // and returning home with preserved state after default handling
     
+    describe('something something with pushed state', function() {
+      var dispatcher = new Dispatcher({ genh: function() { return 'XXXXXXXX' } })
+        , request, response, err;
+    
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+    
+      before(function(done) {
+        // TODO: test case with multiple handlers
+        function handler(req, res, next) {
+          console.log('HANLDER!!!');
+          
+          //req.state.provider = 'https://www.example.com';
+          
+          req.state.push({ provider: 'https://www.example.com' })
+          
+          res.redirect('/login/federated')
+          //next();
+        }
+      
+        chai.express.handler(dispatcher.flow(handler))
+          .req(function(req) {
+            request = req;
+            request.method = 'GET';
+            request.url = '/';
+            request.session = {};
+            request.session.state = {};
+          })
+          .end(function(res) {
+            console.log(request.session)
+            
+            response = res;
+            done();
+          })
+          .next(function(err) {
+            console.log(err)
+          })
+          .dispatch();
+      });
+  
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+  
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(0);
+        expect(dispatcher._store.save).to.have.callCount(1);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(0);
+      });
+    
+      it('should update state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          provider: 'https://www.example.com',
+          returnTo: '/'
+        });
+      });
+    
+      it('should persist state in session', function() {
+        expect(request.session).to.deep.equal({
+          state: {
+            'XXXXXXXX': {
+              provider: 'https://www.example.com',
+              returnTo: '/'
+            }
+          }
+        });
+      });
+    
+      it('should not set locals', function() {
+        expect(request.locals).to.be.undefined;
+      });
+  
+      /*
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.an('object'); // FIXME: remove yieldState
+      });
+      */
+  
+      it('should redirect', function() {
+        expect(response.statusCode).to.equal(302);
+        expect(response.getHeader('Location')).to.equal('/login/federated?state=XXXXXXXX');
+      });
+    }); // something something with pushed state
+    
   }); // redirect back from OAuth service provider
   
 });
