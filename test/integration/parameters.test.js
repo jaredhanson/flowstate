3d4e22853e93b488ea0a97425638e7029f3d8f60
Jared Hanson <jaredhanson@gmail.com>
2020-12-04 15:26:50 -0800

    Stub in parameters test.

---
+++ test/integration/parameters.test.js
@@ -0,0 +1,112 @@
+var chai = require('chai')
+  , expect = require('chai').expect
+  , sinon = require('sinon')
+  , Dispatcher = require('../../lib/manager');
+
+
+describe('integration: parameters', function() {
+  
+  describe('redirect back from OAuth service provider', function() {
+    
+    describe('and returning to report with preserved state and session established by default handler', function() {
+      var dispatcher = new Dispatcher({ genh: function() { return 'XXXXXXXX' } })
+        , request, response, err;
+    
+      before(function() {
+        sinon.spy(dispatcher._store, 'load');
+        sinon.spy(dispatcher._store, 'save');
+        sinon.spy(dispatcher._store, 'update');
+        sinon.spy(dispatcher._store, 'destroy');
+      });
+    
+      before(function(done) {
+        // TODO: test case with multiple handlers
+        function handler(req, res, next) {
+          console.log('HANLDER!!!');
+          
+          req.state.provider = 'https://www.example.com';
+          
+          res.redirect('/login/federated')
+          //next();
+        }
+      
+        chai.express.handler(dispatcher.flow(handler))
+          .req(function(req) {
+            request = req;
+            request.method = 'GET';
+            request.url = '/';
+            request.session = {};
+            request.session.state = {};
+          })
+          .end(function(res) {
+            console.log(request.session)
+            
+            response = res;
+            done();
+          })
+          .next(function(err) {
+            console.log(err)
+          })
+          .dispatch();
+      });
+  
+      after(function() {
+        dispatcher._store.destroy.restore();
+        dispatcher._store.update.restore();
+        dispatcher._store.save.restore();
+        dispatcher._store.load.restore();
+      });
+  
+  
+      /*
+      it('should correctly invoke state store', function() {
+        expect(dispatcher._store.load).to.have.callCount(2);
+        expect(dispatcher._store.save).to.have.callCount(0);
+        expect(dispatcher._store.update).to.have.callCount(0);
+        expect(dispatcher._store.destroy).to.have.callCount(1);
+      });
+      */
+    
+      it('should update state', function() {
+        expect(request.state).to.be.an('object');
+        expect(request.state).to.deep.equal({
+          name: '/',
+          provider: 'https://www.example.com',
+          returnTo: '/'
+        });
+      });
+    
+      /*
+      it('should persist state in session', function() {
+        expect(request.session).to.deep.equal({
+          user: { id: '248289761001' },
+          state: {
+            'Dxh5N7w_wMQ': {
+              accounts: [ { id: '1207059', provider: 'https://www.facebook.com' } ]
+            }
+          }
+        });
+      });
+      */
+    
+      it('should not set locals', function() {
+        expect(request.locals).to.be.undefined;
+      });
+  
+      /*
+      it('should not set yieldState', function() {
+        expect(request.yieldState).to.be.an('object'); // FIXME: remove yieldState
+      });
+      */
+  
+      it('should redirect', function() {
+        expect(response.statusCode).to.equal(302);
+        // TODO: Should have state param here
+        // TODO: should be /login/federated
+        expect(response.getHeader('Location')).to.equal('/login/federated?state=XXXXXXXX');
+      });
+    }); // and returning home with preserved state after default handling
+    
+  }); // redirect back from OAuth service provider
+  
+});
