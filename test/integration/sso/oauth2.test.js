90951a2c0a3d4ef694dc1b98f3e1dbe5fca5a57a
Jared Hanson <jaredhanson@gmail.com>
2021-10-13 07:30:18 -0700

    Remove old dispatcher test.

--- test/integration/sso/oauth2.test.js
+++ test/integration/sso/oauth2.test.js
@@ -1,7 +1,6 @@
 var chai = require('chai')
   , expect = require('chai').expect
   , sinon = require('sinon')
-  , Dispatcher = require('../../../lib/manager')
   , state = require('../../../lib/middleware/state')
   , SessionStore = require('../../../lib/stores/session')
 
@@ -804,114 +803,6 @@ describe('integration: sso/oauth2', function() {
     
     // TODO: Test case for popping return data into state and/or query params
     //       (for example, when a session is not established, but the return to page needs info)
-    
-    // FIXME: make sure this is testing for correct behavior
-    //        broke as a result fo removing _yield from Manager
-     //        (same problem in oauth1 tests)
-    describe.skip('and returning to report with preserved state and session established by default handler', function() {
-      var dispatcher = new Dispatcher()
-        , request, response, err;
-    
-      before(function() {
-        sinon.spy(dispatcher._store, 'load');
-        sinon.spy(dispatcher._store, 'save');
-        sinon.spy(dispatcher._store, 'update');
-        sinon.spy(dispatcher._store, 'destroy');
-      });
-    
-      before(function(done) {
-        // TODO: test case with multiple handlers
-        function handler(req, res, next) {
-          req.federatedUser = { id: '248289761001', provider: 'http://server.example.com' };
-          req.state.complete();
-          next();
-        }
-      
-        function defaultHandler(req, res, next) {
-          console.log('DEFAULT HANDLER IS!');
-          
-          req.session.user = { id: req.federatedUser.id };
-          next();
-        }
-      
-        chai.express.handler(dispatcher.flow(handler, [ defaultHandler ]))
-          .req(function(req) {
-            request = req;
-            request.method = 'GET';
-            request.url = '/oauth2/redirect?code=SplxlOBeZQQYbYS6WxSbIA&state=af0ifjsldkj';
-            request.query = { code: 'SplxlOBeZQQYbYS6WxSbIA', state: 'af0ifjsldkj' };
-            request.session = {};
-            request.session.state = {};
-            request.session.state['af0ifjsldkj'] = {
-              provider: 'http://server.example.com',
-              returnTo: '/report/magic-quadrant',
-              state: 'Dxh5N7w_wMQ'
-            };
-            request.session.state['Dxh5N7w_wMQ'] = {
-              accounts: [ { id: '1207059', provider: 'https://www.facebook.com' } ]
-            };
-          })
-          .end(function(res) {
-            response = res;
-            done();
-          })
-          .next(function(err) {
-            console.log(err)
-          })
-          .dispatch();
-      });
-  
-      after(function() {
-        dispatcher._store.destroy.restore();
-        dispatcher._store.update.restore();
-        dispatcher._store.save.restore();
-        dispatcher._store.load.restore();
-      });
-  
-  
-      it('should correctly invoke state store', function() {
-        expect(dispatcher._store.load).to.have.callCount(2);
-        expect(dispatcher._store.save).to.have.callCount(0);
-        expect(dispatcher._store.update).to.have.callCount(0);
-        expect(dispatcher._store.destroy).to.have.callCount(1);
-      });
-    
-      it('should set properties on request', function() {
-        expect(request.federatedUser).to.be.an('object');
-        expect(request.federatedUser).to.deep.equal({ id: '248289761001', provider: 'http://server.example.com' });
-      });
-    
-      it('should update state', function() {
-        expect(request.state).to.be.an('object');
-        expect(request.state).to.deep.equal({
-          accounts: [ { id: '1207059', provider: 'https://www.facebook.com' } ]
-        });
-      });
-    
-      it('should persist state in session', function() {
-        expect(request.session).to.deep.equal({
-          user: { id: '248289761001' },
-          state: {
-            'Dxh5N7w_wMQ': {
-              accounts: [ { id: '1207059', provider: 'https://www.facebook.com' } ]
-            }
-          }
-        });
-      });
-    
-      it('should not set locals', function() {
-        expect(request.locals).to.be.undefined;
-      });
-  
-      it('should not set yieldState', function() {
-        expect(request.yieldState).to.be.an('object'); // FIXME: remove yieldState
-      });
-  
-      it('should redirect', function() {
-        expect(response.statusCode).to.equal(302);
-        expect(response.getHeader('Location')).to.equal('/report/magic-quadrant?state=Dxh5N7w_wMQ');
-      });
-    }); // and returning to report with preserved state and session established by default handler
   
   }); // redirect back from OAuth 2.0 authorization server
   
