5768ab9ef5ec161ad87b7988ed7c8edb36d291fb
Jared Hanson <jaredhanson@gmail.com>
2021-11-02 16:28:54 -0700

    Clean up tests.

--- test/integration/federated/oauth2.test.js
+++ test/integration/federated/oauth2.test.js
@@ -165,89 +165,72 @@ describe('integration: sso/oauth2', function() {
           done();
         })
         .listen();
-    }); // should consume state from state query parameter and return to location
+    }); // should consume state with state query parameter and return to location
     
-    describe('and returning to location yeilding parameters', function() {
-      var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } })
-        , request, response, err;
-    
-      before(function() {
-        sinon.spy(store, 'load');
-        sinon.spy(store, 'save');
-        sinon.spy(store, 'update');
-        sinon.spy(store, 'destroy');
-      });
-    
-      before(function(done) {
-        function handler(req, res, next) {
-          req.federatedUser = { id: '248289761001', provider: 'http://server.example.com' };
-          res.resumeState({
-            beep: 'boop'
-          }, next);
-        }
-        
-        function redirect(req, res, next) {
-          res.redirect('/home')
-        }
-      
-        chai.express.use([ state({ store: store }), handler, redirect ])
-          .request(function(req) {
-            request = req;
-            request.connection = { encrypted: true };
-            request.method = 'GET';
-            request.url = '/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=af0ifjsldkj';
-            request.headers = {
-              'host': 'client.example.com'
-            }
-            request.query = { code: 'SplxlOBeZQQYbYS6WxSbIA', state: 'af0ifjsldkj' };
-            request.session = {};
-            request.session.state = {};
-            request.session.state['af0ifjsldkj'] = {
-              location: 'https://client.example.com/cb',
-              provider: 'http://server.example.com',
-              returnTo: 'https://client.example.com/'
-            };
-          })
-          .finish(function() {
-            response = this;
-            done();
-          })
-          .listen();
-      });
+    it('and returning to location yeilding parameters', function(done) {
+      var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } });
+      sinon.spy(store, 'load');
+      sinon.spy(store, 'save');
+      sinon.spy(store, 'update');
+      sinon.spy(store, 'destroy');
   
-  
-      it('should correctly invoke state store', function() {
-        expect(store.load).to.have.callCount(1);
-        expect(store.save).to.have.callCount(1);
-        expect(store.update).to.have.callCount(0);
-        // FIXME: destroy shouldn't be called here?
-        expect(store.destroy).to.have.callCount(1);
-      });
-    
-      it('should set state', function() {
-        expect(request.state).to.be.an('object');
-        expect(request.state).to.deep.equal({
-          location: 'https://client.example.com/cb',
-          provider: 'http://server.example.com',
-          returnTo: 'https://client.example.com/'
-        });
-      });
+      function handler(req, res, next) {
+        req.federatedUser = { id: '248289761001', provider: 'http://server.example.com' };
+        res.resumeState({
+          beep: 'boop'
+        }, next);
+      }
       
-      it('should persist state in session', function() {
-        expect(request.session).to.deep.equal({
-          state: {
-            'XXXXXXXX': {
-              location: 'https://client.example.com/',
-              beep: 'boop'
-            }
+      function redirect(req, res, next) {
+        res.redirect('/home')
+      }
+    
+      chai.express.use([ state({ store: store }), handler, redirect ])
+        .request(function(req) {
+          req.connection = { encrypted: true };
+          req.method = 'GET';
+          req.url = '/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=af0ifjsldkj';
+          req.headers = {
+            'host': 'client.example.com'
           }
-        });
-      });
-  
-      it('should redirect', function() {
-        expect(response.statusCode).to.equal(302);
-        expect(response.getHeader('Location')).to.equal('https://client.example.com/?state=XXXXXXXX');
-      });
+          req.query = { code: 'SplxlOBeZQQYbYS6WxSbIA', state: 'af0ifjsldkj' };
+          req.session = {};
+          req.session.state = {};
+          req.session.state['af0ifjsldkj'] = {
+            location: 'https://client.example.com/cb',
+            provider: 'http://server.example.com',
+            returnTo: 'https://client.example.com/'
+          };
+        })
+        .finish(function() {
+          expect(store.load).to.have.callCount(1);
+          expect(store.save).to.have.callCount(1);
+          expect(store.update).to.have.callCount(0);
+          // FIXME: destroy shouldn't be called here?
+          expect(store.destroy).to.have.callCount(1);
+          
+          expect(this.req.state).to.be.an('object');
+          expect(this.req.state).to.deep.equal({
+            location: 'https://client.example.com/cb',
+            provider: 'http://server.example.com',
+            returnTo: 'https://client.example.com/'
+          });
+          
+          expect(this.req.session).to.deep.equal({
+            state: {
+              'XXXXXXXX': {
+                location: 'https://client.example.com/',
+                beep: 'boop'
+              }
+            }
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('https://client.example.com/?state=XXXXXXXX');
+          
+          done();
+        })
+        .listen();
     }); // and returning to location yeilding parameters
     
     describe('and resuming state', function() {
