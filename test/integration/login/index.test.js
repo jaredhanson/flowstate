c11c99cd1fc147afd04f18b170cb78222ad25d54
Jared Hanson <jaredhanson@gmail.com>
2021-11-02 13:56:03 -0700

    Update tests.

--- test/integration/login/index.test.js
+++ test/integration/login/index.test.js
@@ -34,7 +34,6 @@ describe('GET /login', function() {
         expect(store.update).to.have.callCount(0);
         expect(store.destroy).to.have.callCount(0);
         
-        expect(this.req.state).to.be.an('object');
         expect(this.req.state).to.deep.equal({});
         expect(this.req.session).to.deep.equal({});
         
@@ -45,9 +44,8 @@ describe('GET /login', function() {
       .listen();
   }); // should redirect without state parameter
   
-  it('should initialize state with referrer and redirect without state parameter', function(done) {
-    var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } });
-  
+  it('should initialize state with referrer header and redirect without state parameter', function(done) {
+    var store = new SessionStore();
     sinon.spy(store, 'load');
     sinon.spy(store, 'save');
     sinon.spy(store, 'update');
@@ -74,7 +72,6 @@ describe('GET /login', function() {
         expect(store.update).to.have.callCount(0);
         expect(store.destroy).to.have.callCount(0);
         
-        expect(this.req.state).to.be.an('object');
         expect(this.req.state).to.deep.equal({
           returnTo: 'https://client.example.com/'
         });
@@ -85,87 +82,63 @@ describe('GET /login', function() {
         done();
       })
       .listen();
-  }); // should initialize state with referrer and redirect without state parameter
+  }); // should initialize state with referrer header and redirect without state parameter
   
-  describe('with state parameter', function() {
-    var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } })
-      , request, response, err;
-  
-    before(function() {
-      sinon.spy(store, 'load');
-      sinon.spy(store, 'save');
-      sinon.spy(store, 'update');
-      sinon.spy(store, 'destroy');
-    });
-  
-    before(function(done) {
-      function handler(req, res, next) {
-        res.redirect('/login/password');
-      }
-      
-      chai.express.use([ state({ store: store }), handler ])
-        .request(function(req) {
-          req.header = function(name) {
-            var lc = name.toLowerCase();
-            return this.headers[lc];
-          }
-          
-          request = req;
-          request.connection = { encrypted: true };
-          request.method = 'GET';
-          request.url = '/login?state=00000000';
-          request.headers = {
-            'host': 'server.example.com',
-            'referer': 'https://client.example.com/'
-          }
-          request.query = { state: '00000000' };
-          request.session = {};
-          request.session.state = {};
-          request.session.state['00000000'] = {
-            location: 'https://www.example.com/oauth2/authorize/continue',
-            clientID: 's6BhdRkqt3',
-            redirectURI: 'https://client.example.com/cb',
-            state: 'xyz'
-          };
-        })
-        .finish(function() {
-          response = this;
-          done();
-        })
-        .listen();
-    });
+  it('should initialize state with state parameter and redirect with same state parameter', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
 
-    it('should correctly invoke state store', function() {
-      expect(store.load).to.have.callCount(1);
-      expect(store.save).to.have.callCount(0);
-      expect(store.update).to.have.callCount(0);
-      expect(store.destroy).to.have.callCount(0);
-    });
-  
-    it('should set state', function() {
-      expect(request.state).to.be.an('object');
-      expect(request.state).to.deep.equal({
-        state: '00000000'
-      });
-    });
+    function handler(req, res, next) {
+      res.redirect('/login/password');
+    }
     
-    it('should preserve state in session', function() {
-      expect(request.session).to.deep.equal({
-        state: {
-          '00000000': {
-            location: 'https://www.example.com/oauth2/authorize/continue',
-            clientID: 's6BhdRkqt3',
-            redirectURI: 'https://client.example.com/cb',
-            state: 'xyz'
-          }
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req) {
+        req.method = 'GET';
+        req.url = '/login?state=00000000';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://client.example.com/'
         }
-      });
-    });
-
-    it('should redirect', function() {
-      expect(response.statusCode).to.equal(302);
-      expect(response.getHeader('Location')).to.equal('/login/password?state=00000000');
-    });
+        req.connection = { encrypted: true };
+        req.query = { state: '00000000' };
+        req.session = {};
+        req.session.state = {};
+        req.session.state['00000000'] = {
+          location: 'https://www.example.com/oauth2/authorize/continue',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com/cb',
+          state: 'xyz'
+        };
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(1);
+        expect(store.save).to.have.callCount(0);
+        expect(store.update).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.state).to.deep.equal({
+          state: '00000000'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            '00000000': {
+              location: 'https://www.example.com/oauth2/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://client.example.com/cb',
+              state: 'xyz'
+            }
+          }
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login/password?state=00000000');
+        done();
+      })
+      .listen();
   }); // with state parameter
   
 });
