06dc55ac2f2e2557af5b1c5b2aeff8d9d49259f7
Jared Hanson <jaredhanson@gmail.com>
2021-11-02 13:36:49 -0700

    Clean up tests.

--- test/integration/login/index.test.js
+++ test/integration/login/index.test.js
@@ -7,137 +7,85 @@ var chai = require('chai')
 
 describe('GET /login', function() {
     
-  it('without parameters', function(done) {
-    var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } })
-      , request, response, err;
-  
-      sinon.spy(store, 'load');
-      sinon.spy(store, 'save');
-      sinon.spy(store, 'update');
-      sinon.spy(store, 'destroy');
-  
-      function handler(req, res, next) {
-        res.redirect('/login/password');
-      }
-      
-      chai.express.use([ state({ store: store }), handler ])
-        .request(function(req) {
-          req.header = function(name) {
-            var lc = name.toLowerCase();
-            return this.headers[lc];
-          }
-          
-          request = req;
-          request.connection = { encrypted: true };
-          request.method = 'GET';
-          request.url = '/login';
-          request.headers = {
-            'host': 'server.example.com'
-          }
-          request.session = {};
-        })
-        .finish(function() {
-          response = this;
-          
-          expect(store.load).to.have.callCount(0);
-          expect(store.save).to.have.callCount(0);
-          expect(store.update).to.have.callCount(0);
-          expect(store.destroy).to.have.callCount(0);
-          
-          expect(request.state).to.be.an('object');
-          expect(request.state).to.deep.equal({});
-          
-          expect(request.session).to.deep.equal({});
-          
-          expect(response.statusCode).to.equal(302);
-          expect(response.getHeader('Location')).to.equal('/login/password');
-          
-          done();
-        })
-        .listen();
+  it('should initialize state without properties and redirect without state parameter', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
 
-        /*
-    it('should correctly invoke state store', function() {
-      
-    });
-  
-    it('should set state', function() {
-      
-    });
+    function handler(req, res, next) {
+      res.redirect('/login/password');
+    }
     
-    it('should not persist state in session', function() {
-      
-    });
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req) {
+        req.method = 'GET';
+        req.url = '/login';
+        req.headers = {
+          'host': 'server.example.com'
+        }
+        req.connection = { encrypted: true };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(0);
+        expect(store.save).to.have.callCount(0);
+        expect(store.update).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.state).to.be.an('object');
+        expect(this.req.state).to.deep.equal({});
+        expect(this.req.session).to.deep.equal({});
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login/password');
+        done();
+      })
+      .listen();
+  }); // should redirect without state parameter
+  
+  it('should initialize state with referrer and redirect without state parameter', function(done) {
+    var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } });
+  
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
 
-    it('should redirect', function() {
-      
-    });
-        */
-  }); // without parameters
-  
-  describe('with referrer', function() {
-    var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } })
-      , request, response, err;
-  
-    before(function() {
-      sinon.spy(store, 'load');
-      sinon.spy(store, 'save');
-      sinon.spy(store, 'update');
-      sinon.spy(store, 'destroy');
-    });
-  
-    before(function(done) {
-      function handler(req, res, next) {
-        res.redirect('/login/password');
-      }
-      
-      chai.express.use([ state({ store: store }), handler ])
-        .request(function(req) {
-          req.header = function(name) {
-            var lc = name.toLowerCase();
-            return this.headers[lc];
-          }
-          
-          request = req;
-          request.connection = { encrypted: true };
-          request.method = 'GET';
-          request.url = '/login';
-          request.headers = {
-            'host': 'server.example.com',
-            'referer': 'https://client.example.com/'
-          }
-          request.session = {};
-        })
-        .finish(function() {
-          response = this;
-          done();
-        })
-        .listen();
-    });
-
-    it('should correctly invoke state store', function() {
-      expect(store.load).to.have.callCount(0);
-      expect(store.save).to.have.callCount(0);
-      expect(store.update).to.have.callCount(0);
-      expect(store.destroy).to.have.callCount(0);
-    });
-  
-    it('should set state', function() {
-      expect(request.state).to.be.an('object');
-      expect(request.state).to.deep.equal({
-        returnTo: 'https://client.example.com/'
-      });
-    });
+    function handler(req, res, next) {
+      res.redirect('/login/password');
+    }
     
-    it('should not persist state in session', function() {
-      expect(request.session).to.deep.equal({});
-    });
-
-    it('should redirect', function() {
-      expect(response.statusCode).to.equal(302);
-      expect(response.getHeader('Location')).to.equal('/login/password');
-    });
-  }); // with referrer
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req) {
+        req.method = 'GET';
+        req.url = '/login';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://client.example.com/'
+        }
+        req.connection = { encrypted: true };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(0);
+        expect(store.save).to.have.callCount(0);
+        expect(store.update).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.state).to.be.an('object');
+        expect(this.req.state).to.deep.equal({
+          returnTo: 'https://client.example.com/'
+        });
+        expect(this.req.session).to.deep.equal({});
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login/password');
+        done();
+      })
+      .listen();
+  }); // should initialize state with referrer and redirect without state parameter
   
   describe('with state parameter', function() {
     var store = new SessionStore({ genh: function() { return 'XXXXXXXX' } })
