f6466dc32d2f31cbcccac7436c03b3646ff14704
Jared Hanson <jaredhanson@gmail.com>
2021-11-02 14:55:48 -0700

    Clean up tests.

--- test/integration/login/password.test.js
+++ test/integration/login/password.test.js
@@ -231,147 +231,97 @@ describe('GET /login/password', function() {
   
 describe('POST /login/password', function() {
   
-  describe('from same resource as referring page', function() {
+  it('from same resource as referring page', function(done) {
     var store = new SessionStore()
-      , request, response, err;
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
 
-    before(function() {
-      sinon.spy(store, 'load');
-      sinon.spy(store, 'save');
-      sinon.spy(store, 'update');
-      sinon.spy(store, 'destroy');
-    });
+    function handler(req, res, next) {
+      res.resumeState(next);
+    }
+    
+    function redirect(req, res, next) {
+      res.redirect('/home')
+    }
 
-    before(function(done) {
-      function handler(req, res, next) {
-        req.user = { id: '1000', username: 'Aladdin' };
-        res.resumeState(next);
-      }
-      
-      function redirect(req, res, next) {
-        res.redirect('/home')
-      }
-  
-      // TODO: Consider initializing state middleware with a default return to URL
-      chai.express.use([ state({ store: store }), handler, redirect ])
-        .request(function(req, res) {
-          response = res;
-          
-          req.header = function(name) {
-            var lc = name.toLowerCase();
-            return this.headers[lc];
-          }
+    // TODO: Consider initializing state middleware with a default return to URL
+    chai.express.use([ state({ store: store }), handler, redirect ])
+      .request(function(req, res) {
+        req.method = 'POST';
+        req.url = '/login/password';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/login/password'
+        }
+        req.connection = { encrypted: true };
+        req.body = { username: 'Aladdin', password: 'open sesame' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(0);
+        expect(store.save).to.have.callCount(0);
+        expect(store.update).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
         
-          request = req;
-          request.connection = { encrypted: true };
-          request.method = 'POST';
-          request.url = '/login/password';
-          request.headers = {
-            'host': 'www.example.com',
-            'referer': 'https://www.example.com/login/password'
-          }
-          request.body = { username: 'Aladdin', password: 'open sesame' };
-          request.session = {};
-        })
-        .finish(function() {
-          done();
-        })
-        .listen();
-    });
-
-
-    it('should correctly invoke state store', function() {
-      expect(store.load).to.have.callCount(0);
-      expect(store.save).to.have.callCount(0);
-      expect(store.update).to.have.callCount(0);
-      expect(store.destroy).to.have.callCount(0);
-    });
-    
-    it('should set state', function() {
-      expect(request.state).to.be.an('object');
-      expect(request.state).to.deep.equal({});
-    });
-    
-    it('should not persist state in session', function() {
-      expect(request.session).to.deep.equal({});
-    });
-
-    it('should redirect', function() {
-      expect(response.statusCode).to.equal(302);
-      expect(response.getHeader('Location')).to.equal('/home');
-    });
+        expect(this.req.state).to.deep.equal({});
+        expect(this.req.session).to.deep.equal({});
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/home');
+        done();
+      })
+      .listen();
   }); // from same resource as referring page
   
-  describe('with return_to parameter', function() {
-    var store = new SessionStore()
-      , request, response, err;
+  it('with return_to parameter', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
 
-    before(function() {
-      sinon.spy(store, 'load');
-      sinon.spy(store, 'save');
-      sinon.spy(store, 'update');
-      sinon.spy(store, 'destroy');
-    });
+    function handler(req, res, next) {
+      req.user = { id: '1000', username: 'Aladdin' };
+      res.resumeState(next);
+    }
+    
+    function redirect(req, res, next) {
+      res.redirect('/home')
+    }
 
-    before(function(done) {
-      function handler(req, res, next) {
-        req.user = { id: '1000', username: 'Aladdin' };
-        res.resumeState(next);
-      }
-      
-      function redirect(req, res, next) {
-        res.redirect('/home')
-      }
-  
-      chai.express.use([ state({ store: store }), handler, redirect ])
-        .request(function(req, res) {
-          response = res;
-          
-          req.header = function(name) {
-            var lc = name.toLowerCase();
-            return this.headers[lc];
-          }
+    chai.express.use([ state({ store: store }), handler, redirect ])
+      .request(function(req, res) {
+        req.method = 'POST';
+        req.url = '/login/password';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/login/password'
+        }
+        req.connection = { encrypted: true };
+        req.body = { username: 'Aladdin', password: 'open sesame', return_to: 'https://www.example.com/' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(0);
+        expect(store.save).to.have.callCount(0);
+        expect(store.update).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
         
-          request = req;
-          request.connection = { encrypted: true };
-          request.method = 'POST';
-          request.url = '/login/password';
-          request.headers = {
-            'host': 'www.example.com',
-            'referer': 'https://www.example.com/login/password'
-          }
-          request.body = { username: 'Aladdin', password: 'open sesame', return_to: 'https://www.example.com/' };
-          request.session = {};
-        })
-        .finish(function() {
-          done();
-        })
-        .listen();
-    });
-
-
-    it('should correctly invoke state store', function() {
-      expect(store.load).to.have.callCount(0);
-      expect(store.save).to.have.callCount(0);
-      expect(store.update).to.have.callCount(0);
-      expect(store.destroy).to.have.callCount(0);
-    });
-    
-    it('should set state', function() {
-      expect(request.state).to.be.an('object');
-      expect(request.state).to.deep.equal({
-        returnTo: 'https://www.example.com/'
-      });
-    });
-    
-    it('should not persist state in session', function() {
-      expect(request.session).to.deep.equal({});
-    });
-
-    it('should redirect', function() {
-      expect(response.statusCode).to.equal(302);
-      expect(response.getHeader('Location')).to.equal('https://www.example.com/');
-    });
+        expect(this.req.state).to.be.an('object');
+        expect(this.req.state).to.deep.equal({
+          returnTo: 'https://www.example.com/'
+        });
+        
+        expect(this.req.session).to.deep.equal({});
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://www.example.com/');
+        
+        done();
+      })
+      .listen();
   }); // with return_to parameter
   
   describe('with state parameter', function() {
