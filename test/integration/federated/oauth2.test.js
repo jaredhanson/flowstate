6ee03e79fd67d308f35d4464b8482bee5952b4e4
Jared Hanson <jaredhanson@gmail.com>
2021-11-02 18:25:17 -0700

    Clean up tests.

--- test/integration/federated/oauth2.test.js
+++ test/integration/federated/oauth2.test.js
@@ -234,93 +234,74 @@ describe('integration: sso/oauth2', function() {
         .listen();
     }); // and returning to location yeilding parameters
     
-    describe('and resuming state', function() {
-      var store = new SessionStore()
-        , request, response, err;
-    
-      before(function() {
-        sinon.spy(store, 'load');
-        sinon.spy(store, 'save');
-        sinon.spy(store, 'update');
-        sinon.spy(store, 'destroy');
-      });
-    
-      before(function(done) {
-        // TODO: test case with multiple handlers
-        function handler(req, res, next) {
-          req.federatedUser = { id: '248289761001', provider: 'http://server.example.com' };
-          res.resumeState(next);
-        }
-        
-        function redirect(req, res, next) {
-          res.redirect('/home')
-        }
+    it('should consume state with state query parameter and resume state', function(done) {
+      var store = new SessionStore();
+      sinon.spy(store, 'load');
+      sinon.spy(store, 'save');
+      sinon.spy(store, 'update');
+      sinon.spy(store, 'destroy');
+  
+      // TODO: test case with multiple handlers
+      function handler(req, res, next) {
+        res.resumeState(next);
+      }
       
-        chai.express.use([ state({ store: store }), handler, redirect ])
-          .request(function(req) {
-            request = req;
-            request.connection = { encrypted: true };
-            request.method = 'GET';
-            request.url = '/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=af0ifjsldkj';
-            request.headers = {
-              'host': 'client.example.com'
-            }
-            request.query = { code: 'SplxlOBeZQQYbYS6WxSbIA', state: 'af0ifjsldkj' };
-            request.session = {};
-            request.session.state = {};
-            request.session.state['af0ifjsldkj'] = {
-              location: 'https://client.example.com/cb',
-              provider: 'http://server.example.com',
-              state: 'Dxh5N7w_wMQ'
-            };
-            request.session.state['Dxh5N7w_wMQ'] = {
-              location: 'https://client.example.com/oauth2/authorize/continue',
-              clientID: 's6BhdRkqt3',
-              redirectURI: 'https://client.example.com/cb',
-              state: 'xyz'
-            };
-          })
-          .finish(function() {
-            response = this;
-            done();
-          })
-          .listen();
-      });
-  
-  
-      it('should correctly invoke state store', function() {
-        expect(store.load).to.have.callCount(2);
-        expect(store.save).to.have.callCount(0);
-        expect(store.update).to.have.callCount(0);
-        expect(store.destroy).to.have.callCount(1);
-      });
+      function redirect(req, res, next) {
+        res.redirect('/home')
+      }
     
-      it('should set state', function() {
-        expect(request.state).to.be.an('object');
-        expect(request.state).to.deep.equal({
-          location: 'https://client.example.com/cb',
-          provider: 'http://server.example.com',
-          state: 'Dxh5N7w_wMQ'
-        });
-      });
-    
-      it('should remove state from session', function() {
-        expect(request.session).to.deep.equal({
-          state: {
-            'Dxh5N7w_wMQ': {
-              location: 'https://client.example.com/oauth2/authorize/continue',
-              clientID: 's6BhdRkqt3',
-              redirectURI: 'https://client.example.com/cb',
-              state: 'xyz'
-            }
+      chai.express.use([ state({ store: store }), handler, redirect ])
+        .request(function(req, res) {
+          req.method = 'GET';
+          req.url = '/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz';
+          req.headers = {
+            'host': 'server.example.com'
           }
-        });
-      });
-  
-      it('should redirect', function() {
-        expect(response.statusCode).to.equal(302);
-        expect(response.getHeader('Location')).to.equal('https://client.example.com/oauth2/authorize/continue?state=Dxh5N7w_wMQ');
-      });
+          req.connection = { encrypted: true };
+          req.query = { code: 'SplxlOBeZQQYbYS6WxSbIA', state: 'xyz' };
+          req.session = {};
+          req.session.state = {};
+          req.session.state['xyz'] = {
+            location: 'https://server.example.com/cb',
+            provider: 'https://server.example.net',
+            state: '00000000'
+          };
+          req.session.state['00000000'] = {
+            location: 'https://server.example.com/authorize/continue',
+            clientID: 's6BhdRkqt3',
+            redirectURI: 'https://client.example.com/cb',
+            state: 'xyz'
+          };
+        })
+        .finish(function() {
+          expect(store.load).to.have.callCount(2);
+          expect(store.save).to.have.callCount(0);
+          expect(store.update).to.have.callCount(0);
+          expect(store.destroy).to.have.callCount(1);
+          
+          expect(this.req.state).to.be.an('object');
+          expect(this.req.state).to.deep.equal({
+            location: 'https://server.example.com/cb',
+            provider: 'https://server.example.net',
+            state: '00000000'
+          });
+          
+          expect(this.req.session).to.deep.equal({
+            state: {
+              '00000000': {
+                location: 'https://server.example.com/authorize/continue',
+                clientID: 's6BhdRkqt3',
+                redirectURI: 'https://client.example.com/cb',
+                state: 'xyz'
+              }
+            }
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('https://server.example.com/authorize/continue?state=00000000');
+          done();
+        })
+        .listen();
     }); // and resuming state
     
     describe('and resuming state yeilding parameters', function() {
