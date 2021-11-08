a1f3baf81281cb3a1327fc0f5ebc6931d7d04a32
Jared Hanson <jaredhanson@gmail.com>
2021-11-08 11:28:18 -0800

    Add tests.

--- test/integration/login/federated.test.js
+++ test/integration/login/federated.test.js
@@ -275,4 +275,152 @@ describe('GET /login/federated', function() {
       .listen();
   }); // should initialize state with state query parameter in preference to return to query parameter and redirect with pushed state
   
+  it('should initialize state with state query parameter and redirect with saved pushed state', function(done) {
+    var store = new SessionStore({ genh: function() { return 'xyz' } });
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
+
+    function handler(req, res, next) {
+      expect(req.state).to.deep.equal({
+        location: 'https://server.example.com/login/federated',
+        resumeState: '00000000'
+      });
+      
+      req.pushState({
+        provider: 'https://server.example.net'
+      }, 'https://server.example.com/cb', function(err, h) {
+        if (err) { return next(err); }
+        res.redirect('https://server.example.net/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fserver.example.com%2Fcb&state=' + h);
+      });
+    }
+
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.method = 'GET';
+        req.url = '/login/federated?provider=https%3A%2F%2Fserver.example.net&state=00000000';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://server.example.com/login?state=00000000'
+        }
+        req.connection = { encrypted: true };
+        req.query = { provider: 'https://server.example.net', state: '00000000' };
+        req.session = {};
+        req.session.state = {};
+        req.session.state['00000000'] = {
+          location: 'https://server.example.com/authorize/continue',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com/cb',
+          state: 'xyz'
+        };
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(1);
+        expect(store.save).to.have.callCount(1);
+        expect(store.update).to.have.callCount(1); // FIXME: this should be 0
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.state).to.deep.equal({
+          location: 'https://server.example.com/cb',
+          provider: 'https://server.example.net',
+          resumeState: '00000000'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            '00000000': {
+              location: 'https://server.example.com/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://client.example.com/cb',
+              state: 'xyz'
+            },
+            'xyz': {
+              location: 'https://server.example.com/cb',
+              provider: 'https://server.example.net',
+              resumeState: '00000000'
+            }
+          }
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://server.example.net/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fserver.example.com%2Fcb&state=xyz');
+        done();
+      })
+      .listen();
+  }); // should initialize state with state query parameter and redirect with saved pushed state
+  
+  it('should initialize state with state query parameter and redirect with saved pushed state using handle option', function(done) {
+    var store = new SessionStore({ genh: function() { return 'xyz' } });
+    sinon.spy(store, 'load');
+    sinon.spy(store, 'save');
+    sinon.spy(store, 'update');
+    sinon.spy(store, 'destroy');
+
+    function handler(req, res, next) {
+      expect(req.state).to.deep.equal({
+        location: 'https://server.example.com/login/federated',
+        resumeState: '00000000'
+      });
+      
+      req.pushState({
+        provider: 'https://server.example.net'
+      }, 'https://server.example.com/cb', { handle: 'oauth2:123' }, function(err, h) {
+        if (err) { return next(err); }
+        res.redirect('https://server.example.net/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fserver.example.com%2Fcb&state=' + h);
+      });
+    }
+
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.method = 'GET';
+        req.url = '/login/federated?provider=https%3A%2F%2Fserver.example.net&state=00000000';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://server.example.com/login?state=00000000'
+        }
+        req.connection = { encrypted: true };
+        req.query = { provider: 'https://server.example.net', state: '00000000' };
+        req.session = {};
+        req.session.state = {};
+        req.session.state['00000000'] = {
+          location: 'https://server.example.com/authorize/continue',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com/cb',
+          state: 'xyz'
+        };
+      })
+      .finish(function() {
+        expect(store.load).to.have.callCount(1);
+        expect(store.save).to.have.callCount(1);
+        expect(store.update).to.have.callCount(1); // FIXME: this should be 0
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.state).to.deep.equal({
+          location: 'https://server.example.com/cb',
+          provider: 'https://server.example.net',
+          resumeState: '00000000'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            '00000000': {
+              location: 'https://server.example.com/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://client.example.com/cb',
+              state: 'xyz'
+            },
+            'oauth2:123': {
+              location: 'https://server.example.com/cb',
+              provider: 'https://server.example.net',
+              resumeState: '00000000'
+            }
+          }
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://server.example.net/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fserver.example.com%2Fcb&state=oauth2%3A123');
+        done();
+      })
+      .listen();
+  }); // should initialize state with state query parameter and redirect with saved pushed state using handle option
+  
 });
