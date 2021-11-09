7df6a2fa9531cdc5eb90aebc6c1fc31b7f3bd790
Jared Hanson <jaredhanson@gmail.com>
2021-11-09 10:24:07 -0800

    Rename stores directory to store.

--- test/integration/federated/oauth2.test.js
+++ test/integration/federated/oauth2.test.js
@@ -2,7 +2,7 @@ var chai = require('chai')
   , expect = require('chai').expect
   , sinon = require('sinon')
   , state = require('../../../lib/middleware/state')
-  , SessionStore = require('../../../lib/stores/session')
+  , SessionStore = require('../../../lib/store/session')
 
   // TODO: Move this to login/federated
 describe('integration: sso/oauth2', function() {
--- test/integration/login/password.test.js
+++ test/integration/login/password.test.js
@@ -2,7 +2,7 @@ var chai = require('chai')
   , expect = require('chai').expect
   , sinon = require('sinon')
   , state = require('../../../lib/middleware/state')
-  , SessionStore = require('../../../lib/stores/session');
+  , SessionStore = require('../../../lib/store/session');
 
 
 describe('GET /login/password', function() {
