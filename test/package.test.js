17905a6b55fd5ec9e6c1e762aba5a02376d52230
Jared Hanson <jaredhanson@gmail.com>
2021-11-11 10:54:49 -0800

    Clean up exports.

--- test/package.test.js
+++ test/package.test.js
@@ -7,7 +7,12 @@ var flowstate = require('..');
 
 describe('flowstate', function() {
   
-  it('should export constructors', function() {
+  it('should export middleware', function() {
+    expect(flowstate).to.be.a('function');
+    expect(flowstate.clean).to.be.a('function');
+  });
+  
+  it('should export stores', function() {
     expect(flowstate.SessionStore).to.be.a('function');
   });
   
