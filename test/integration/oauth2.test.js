eeb202ec063c4893d25de752fe7d80282be937fa
Jared Hanson <jaredhanson@gmail.com>
2021-12-13 14:11:53 -0800

    Add req.popState().

--- test/integration/oauth2.test.js
+++ test/integration/oauth2.test.js
@@ -109,4 +109,150 @@ describe('GET /oauth2/authorize', function() {
       .listen();
   }); // should initialize state by ignoring external state and redirect with pushed state
   
+  it('should initialize state by ignoring external state and respond after completing state', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+
+    function handler(req, res, next) {
+      expect(req.state).to.deep.equal({
+        location: 'https://server.example.com/authorize',
+        returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+      });
+      
+      req.state.complete();
+      res.redirect('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+    }
+  
+    chai.express.use([ state({ external: true, store: store, genh: function() { return '00000000' } }), handler ])
+      .request(function(req, res) {
+        req.method = 'GET';
+        req.url = '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://client.example.com/'
+        }
+        req.connection = { encrypted: true };
+        req.query = { response_type: 'code', client_id: 's6BhdRkqt3', state: 'xyz', redirect_uri: 'https://client.example.com/cb' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.get).to.have.callCount(0);
+        expect(store.set).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.session).to.deep.equal({});
+        expect(this.req.state).to.deep.equal({
+          location: 'https://server.example.com/authorize',
+          returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+        done();
+      })
+      .listen();
+  }); // should initialize state by ignoring external state and respond after completing state
+  
+  it('should initialize state by ignoring external state and respond after popping and completing state', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+
+    function handler(req, res, next) {
+      expect(req.state).to.deep.equal({
+        location: 'https://server.example.com/authorize',
+        returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+      });
+      
+      req.popState();
+      req.state.complete();
+      res.redirect('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+    }
+  
+    chai.express.use([ state({ external: true, store: store, genh: function() { return '00000000' } }), handler ])
+      .request(function(req, res) {
+        req.method = 'GET';
+        req.url = '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://client.example.com/'
+        }
+        req.connection = { encrypted: true };
+        req.query = { response_type: 'code', client_id: 's6BhdRkqt3', state: 'xyz', redirect_uri: 'https://client.example.com/cb' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.get).to.have.callCount(0);
+        expect(store.set).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.session).to.deep.equal({});
+        expect(this.req.state).to.deep.equal({
+          location: 'https://server.example.com/authorize',
+          returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+        done();
+      })
+      .listen();
+  }); // should initialize state by ignoring external state and respond after popping and completing state
+  
+  it('should initialize state by ignoring external state and respond after pushing, popping, and completing state', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+
+    function handler(req, res, next) {
+      expect(req.state).to.deep.equal({
+        location: 'https://server.example.com/authorize',
+        returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+      });
+      
+      req.pushState({
+        clientID: req.query.client_id,
+        redirectURI: req.query.redirect_uri,
+        state: req.query.state
+      }, '/authorize/continue');
+      
+      req.popState();
+      req.state.complete();
+      res.redirect('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+    }
+  
+    chai.express.use([ state({ external: true, store: store, genh: function() { return '00000000' } }), handler ])
+      .request(function(req, res) {
+        req.method = 'GET';
+        req.url = '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb';
+        req.headers = {
+          'host': 'server.example.com',
+          'referer': 'https://client.example.com/'
+        }
+        req.connection = { encrypted: true };
+        req.query = { response_type: 'code', client_id: 's6BhdRkqt3', state: 'xyz', redirect_uri: 'https://client.example.com/cb' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(store.get).to.have.callCount(0);
+        expect(store.set).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        expect(this.req.session).to.deep.equal({});
+        expect(this.req.state).to.deep.equal({
+          location: 'https://server.example.com/authorize',
+          returnTo: 'https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb'
+        });
+        
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://client.example.com/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz');
+        done();
+      })
+      .listen();
+  }); // should initialize state by ignoring external state and respond after pushing, popping, and completing state
+  
 }); // GET /oauth2/authorize
