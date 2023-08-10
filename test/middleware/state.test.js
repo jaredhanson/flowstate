f446c90b75bae4a6e82f6bb25a14205834161440
Jared Hanson <jaredhanson@gmail.com>
2023-08-10 16:35:43 -0700

    Add tests.

--- test/middleware/state.test.js
+++ test/middleware/state.test.js
@@ -63,6 +63,64 @@ describe('middleware/state', function() {
       .listen();
   }); // should initialize state with return URL from referrer header
   
+  it('should initialize state with return URL from query parameter', function(done) {
+    var store = new SessionStore();
+  
+    chai.express.use([ state({ store: store }) ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login?return_to=https%3A%2F%2Fwww.example.com%2Fwelcome';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = { return_to: 'https://www.example.com/welcome' };
+        req.session = {};
+      })
+      .next(function(err, req, res) {
+        if (err) { return done(err); }
+        
+        expect(req.state.isNew()).to.be.true;
+        expect(req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/welcome'
+        });
+        expect(req.stateStore).to.equal(store);
+        done();
+      })
+      .listen();
+  }); // should initialize state with return URL from query parameter
+  
+  it('should initialize state with return URL from body parameter', function(done) {
+    var store = new SessionStore();
+  
+    chai.express.use([ state({ store: store }) ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'POST';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.body = { return_to: 'https://www.example.com/bienvenido' };
+        req.session = {};
+      })
+      .next(function(err, req, res) {
+        if (err) { return done(err); }
+        
+        expect(req.state.isNew()).to.be.true;
+        expect(req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/bienvenido'
+        });
+        expect(req.stateStore).to.equal(store);
+        done();
+      })
+      .listen();
+  }); // should initialize state with return URL from body parameter
+  
   it('should initialize external state', function(done) {
     var store = new SessionStore();
   
