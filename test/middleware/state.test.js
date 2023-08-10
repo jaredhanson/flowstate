20920bcc82b972ff622868213aec3915ba46fee8
Jared Hanson <jaredhanson@gmail.com>
2023-08-10 15:59:16 -0700

    Add tests.

--- test/middleware/state.test.js
+++ test/middleware/state.test.js
@@ -32,6 +32,62 @@ describe('middleware/state', function() {
         done();
       })
       .listen();
-  }); // should redirect with return location
+  }); // should initialize state
+  
+  it('should initialize external state', function(done) {
+    var store = new SessionStore();
+  
+    chai.express.use([ state({ external: true, store: store }) ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/authorize?client_id=s6BhdRkqt3';
+        req.headers = {
+          'host': 'server.example.com'
+        };
+        req.query = { client_id: 's6BhdRkqt3' };
+        req.session = {};
+      })
+      .next(function(err, req, res) {
+        if (err) { return done(err); }
+        
+        expect(req.state.isNew()).to.be.true;
+        expect(req.state).to.deep.equal({
+          location: 'https://server.example.com/authorize',
+          returnTo: 'https://server.example.com/authorize?client_id=s6BhdRkqt3'
+        });
+        expect(req.stateStore).to.equal(store);
+        done();
+      })
+      .listen();
+  }); // should initialize external state
+  
+  it('should initialize external state with return to URL containing state parameter', function(done) {
+    var store = new SessionStore();
+  
+    chai.express.use([ state({ external: true, store: store }) ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/authorize?client_id=s6BhdRkqt3&state=xyz';
+        req.headers = {
+          'host': 'server.example.com'
+        };
+        req.query = { client_id: 's6BhdRkqt3', state: 'xyz' };
+        req.session = {};
+      })
+      .next(function(err, req, res) {
+        if (err) { return done(err); }
+        
+        expect(req.state.isNew()).to.be.true;
+        expect(req.state).to.deep.equal({
+          location: 'https://server.example.com/authorize',
+          returnTo: 'https://server.example.com/authorize?client_id=s6BhdRkqt3&state=xyz'
+        });
+        expect(req.stateStore).to.equal(store);
+        done();
+      })
+      .listen();
+  }); // sshould initialize external state with return to URL containing state parameter
   
 });
