2a45b3c9fe0cb799d261e4284607b02b588e5e5c
Jared Hanson <jaredhanson@gmail.com>
2023-08-10 09:55:02 -0700

    Parse expires to date in State constructor, stringify when setting state.

--- test/store/session.test.js
+++ test/store/session.test.js
@@ -1,32 +1,106 @@
 var chai = require('chai')
   , expect = require('chai').expect
   , sinon = require('sinon')
-  , SessionStore = require('../../lib/store/session');
+  , SessionStore = require('../../lib/store/session')
+  , State = require('../../lib/state');
 
 
 describe('SessionStore', function() {
   
-  describe('#get', function() {
-    var store = new SessionStore();
-    
-    it('should do something', function(done) {
-      var req = new Object();
-      req.session = {};
-      req.session.state = {};
-      req.session.state['xyz'] = {
-        cow: 'moo'
-      };
+  describe('defaults', function() {
   
-      store.get(req, 'xyz', function(err, state) {
-        expect(err).to.be.null;
-        expect(state).to.deep.equal({
-          cow: 'moo'
-        });
-        expect(state).to.not.equal(req.session.state['xyz']);
-        done();
-      });
-    }); // should resolve A record of node
+    describe('#get', function() {
     
-  }); // #get
+      it('should get state', function(done) {
+        var req = new Object();
+        req.session = {};
+        req.session.state = {};
+        req.session.state['xyz'] = {
+          cow: 'moo'
+        };
+  
+        var store = new SessionStore();
+        store.get(req, 'xyz', function(err, state) {
+          expect(err).to.be.null;
+          expect(state).to.deep.equal({
+            cow: 'moo'
+          });
+          expect(state).to.not.equal(req.session.state['xyz']);
+          done();
+        });
+      }); // should get session
+    
+      it('should not get state if session does not exist', function(done) {
+        var req = new Object();
+  
+        var store = new SessionStore();
+        store.get(req, 'xyz', function(err, state) {
+          expect(err).to.be.undefined;
+          expect(state).to.be.undefined;
+          done();
+        });
+      }); // should not get state if session does not exist
+    
+      it('should not get state if state does not exist', function(done) {
+        var req = new Object();
+        req.session = {};
+        req.session.state = {};
+  
+        var store = new SessionStore();
+        store.get(req, 'xyz', function(err, state) {
+          expect(err).to.be.undefined;
+          expect(state).to.be.undefined;
+          done();
+        });
+      }); // should not get state if session does not exist
+    
+      it('should not get state if no state exists', function(done) {
+        var req = new Object();
+        req.session = {};
+  
+        var store = new SessionStore();
+        store.get(req, 'xyz', function(err, state) {
+          expect(err).to.be.undefined;
+          expect(state).to.be.undefined;
+          done();
+        });
+      }); // should not get state if no state exists
+    
+      it('should not get state if no session exists', function(done) {
+        var req = new Object();
+  
+        var store = new SessionStore();
+        store.get(req, 'xyz', function(err, state) {
+          expect(err).to.be.undefined;
+          expect(state).to.be.undefined;
+          done();
+        });
+      }); // should not get state if no session exists
+    
+    }); // #get
+    
+    describe('#set', function() {
+      
+      it('should set state', function(done) {
+        var req = new Object();
+        req.session = {};
+        req.session.state = {};
+        
+        var state = new State(req, { cow: 'moo' });
+  
+        var store = new SessionStore();
+        store.set(req, 'xyz', state, function(err) {
+          expect(err).to.be.null;
+          expect(req.session.state['xyz']).to.deep.equal({
+            cow: 'moo'
+          });
+          expect(req.session.state['xyz']).to.not.be.an.instanceOf(State);
+          done();
+        });
+      }); // should set state
+      
+    }); // #set
+    
+  }); // defaults
   
 });
