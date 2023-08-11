c2b6eb7cd125c8e353ebe7e62d62b49a6c044e67
Jared Hanson <jaredhanson@gmail.com>
2023-08-11 10:33:53 -0700

    Add tests.

--- test/response/render.test.js
+++ test/response/render.test.js
@@ -37,4 +37,36 @@ describe('ServerResponse#render', function() {
       .listen();
   }); // should render
   
+  it('should render with returnTo set to referrer', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.render('login')
+    }
+  
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = {};
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this).to.render('login')
+                    .with.deep.locals({ returnTo: 'https://www.example.com/' });
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/'
+        });
+        expect(this.req.session).to.deep.equal({})
+        done();
+      })
+      .listen();
+  }); // should render with returnTo set to referrer
+  
 });
