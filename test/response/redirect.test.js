817b9ac52fff47c2a0c10363462c111c26053204
Jared Hanson <jaredhanson@gmail.com>
2023-08-11 14:46:22 -0700

    Add tests.

--- test/response/redirect.test.js
+++ test/response/redirect.test.js
@@ -69,4 +69,68 @@ describe('ServerResponse#redirect', function() {
       .listen();
   }); // should redirect with return_to set to referrer
   
+  it('should redirect with return_to set to URL specified by query parameter', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.redirect('/login/password')
+    }
+  
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login?return_to=https%3A%2F%2Fwww.example.com%2Fwelcome';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = { return_to: 'https://www.example.com/welcome' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login/password?return_to=https%3A%2F%2Fwww.example.com%2Fwelcome');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/welcome'
+        });
+        expect(this.req.session).to.deep.equal({});
+        done();
+      })
+      .listen();
+  }); // should redirect with return_to set to URL specified by query parameter
+  
+  it('should redirect with return_to set to URL specified by body parameter', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.redirect('/login')
+    }
+  
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'POST';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.body = { return_to: 'https://www.example.com/bienvenido' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login?return_to=https%3A%2F%2Fwww.example.com%2Fbienvenido');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/bienvenido'
+        });
+        expect(this.req.session).to.deep.equal({});
+        done();
+      })
+      .listen();
+  }); // should redirect with return_to set to URL specified by body parameter
+  
 });
