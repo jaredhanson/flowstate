51219e501930ce7ffa4358aa816e85334738be86
Jared Hanson <jaredhanson@gmail.com>
2023-08-11 14:42:44 -0700

    Add tests.

--- test/response/redirect.test.js
+++ test/response/redirect.test.js
@@ -37,4 +37,36 @@ describe('ServerResponse#redirect', function() {
       .listen();
   }); // should redirect without state
   
+  it('should redirect with return_to set to referrer', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.redirect('/login/password')
+    }
+  
+    chai.express.use([ state({ store: store }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = {};
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/login/password?return_to=https%3A%2F%2Fwww.example.com%2F');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/'
+        });
+        expect(this.req.session).to.deep.equal({});
+        done();
+      })
+      .listen();
+  }); // should redirect with return_to set to referrer
+  
 });
