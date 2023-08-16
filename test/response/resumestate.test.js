fa582eecd412209b1b7b45e761bd9b72b5a9e7af
Jared Hanson <jaredhanson@gmail.com>
2023-08-16 09:58:36 -0700

    Add tests.

--- test/response/resumestate.test.js
+++ test/response/resumestate.test.js
@@ -41,6 +41,41 @@ describe('ServerResponse#resumeState', function() {
       .listen();
   }); // should not resume without state
   
+  it('should resume state by redirecting to redirect URL', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.resumeState(next);
+    }
+  
+    function home(req, res, next) {
+      res.redirect('/home')
+    }
+  
+    chai.express.use([ state({ store: store }), handler, home ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'POST';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com'
+        };
+        req.body = { return_to: 'https://www.example.com/bienvenido' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://www.example.com/bienvenido');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          returnTo: 'https://www.example.com/bienvenido'
+        });
+        expect(this.req.session).to.deep.equal({});
+        done();
+      })
+      .listen();
+  }); //should resume state by redirecting to redirect URL
+  
   it('should redirect to URL to return to', function(done) {
     var store = new SessionStore();
   
