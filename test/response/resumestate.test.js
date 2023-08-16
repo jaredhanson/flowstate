af0f1cf5ffe719a08df78ac19fb74de11e053d12
Jared Hanson <jaredhanson@gmail.com>
2023-08-16 10:33:28 -0700

    Add todo items, test cases.

--- test/response/resumestate.test.js
+++ test/response/resumestate.test.js
@@ -112,6 +112,59 @@ describe('ServerResponse#resumeState', function() {
       .listen();
   }); //should resume state by redirecting to redirect URL with state
   
+  it('should yield', function(done) {
+    var store = new SessionStore();
+  
+    function handler(req, res, next) {
+      res.resumeState({ authuser: '1' }, next);
+    }
+  
+    function home(req, res, next) {
+      res.redirect('/home')
+    }
+  
+    chai.express.use([ state({ store: store }), handler, home ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'POST';
+        req.url = '/account/select';
+        req.headers = {
+          'host': 'www.example.com'
+        };
+        req.body = { return_to: 'https://www.example.com/authorize/continue', state: '123' };
+        req.session = {};
+        req.session.state = {};
+        req.session.state['123'] = {
+          location: 'https://www.example.com/authorize/continue',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://www.example.com/dashboard/cb',
+          state: 'xyz'
+        };
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        // FIXME: this shouldn't have return_to in it
+        expect(this.getHeader('Location')).to.equal('https://www.example.com/authorize/continue?authuser=1&return_to=https%3A%2F%2Fwww.example.com%2Fauthorize%2Fcontinue&state=123');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/account/select',
+          returnTo: 'https://www.example.com/authorize/continue',
+          state: '123'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            '123': {
+              location: 'https://www.example.com/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://www.example.com/dashboard/cb',
+              state: 'xyz'
+            }
+          }
+        });
+        done();
+      })
+      .listen();
+  }); //should yield
+  
   it('should redirect to URL to return to', function(done) {
     var store = new SessionStore();
   
