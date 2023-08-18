ab39d30c4358de4b107f456bee497b03fec26431
Jared Hanson <jaredhanson@gmail.com>
2023-08-18 14:13:36 -0700

    Add tests.

--- test/response/redirect.test.js
+++ test/response/redirect.test.js
@@ -320,6 +320,71 @@ describe('ServerResponse#redirect', function() {
       .listen();
   }); // should redirect with propagated body parameters as redirect URL with state when that state is not found in state store
   
+  it('should redirect with current URL and modified initial state when processing a non-mutating request', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+  
+    function handler(req, res, next) {
+      req.state.riskScore = 0.82;
+      res.redirect('/captcha')
+    }
+  
+    chai.express.use([ state({ store: store, genh: function() { return '456' } }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login?return_to=https%3A%2F%2Fwww.example.com%2Fauthorize%2Fcontinue&state=123';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/login'
+        };
+        req.query = { return_to: 'https://www.example.com/authorize/continue', state: '123' };
+        req.session = {};
+        req.session.state = {};
+        req.session.state['123'] = {
+          location: 'https://www.example.com/authorize/continue',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://www.example.com/dashboard/cb',
+          state: 'xyz'
+        };
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/captcha?return_to=https%3A%2F%2Fwww.example.com%2Flogin&state=456');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login',
+          riskScore: 0.82,
+          returnTo: 'https://www.example.com/authorize/continue',
+          state: '123'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            '456': {
+              location: 'https://www.example.com/login',
+              riskScore: 0.82,
+              returnTo: 'https://www.example.com/authorize/continue',
+              state: '123'
+            },
+            '123': {
+              location: 'https://www.example.com/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://www.example.com/dashboard/cb',
+              state: 'xyz'
+            }
+          }
+        });
+        
+        expect(store.get).to.have.callCount(1);
+        expect(store.set).to.have.callCount(1);
+        expect(store.destroy).to.have.callCount(0);
+        
+        done();
+      })
+      .listen();
+  }); // should redirect with current URL and modified initial state when processing a non-mutating request
+  
   it('should redirect with current URL and state when processing a non-mutating request', function(done) {
     var store = new SessionStore();
     sinon.spy(store, 'get');
@@ -352,7 +417,7 @@ describe('ServerResponse#redirect', function() {
         req.session.state['123'] = {
           location: 'https://www.example.com/authorize/continue',
           clientID: 's6BhdRkqt3',
-          redirectURI: 'https://www.example.com/dashboard/cb',
+          redirectURI: 'https://www.example.com/cb',
           state: 'xyz'
         };
       })
@@ -378,7 +443,7 @@ describe('ServerResponse#redirect', function() {
             '123': {
               location: 'https://www.example.com/authorize/continue',
               clientID: 's6BhdRkqt3',
-              redirectURI: 'https://www.example.com/dashboard/cb',
+              redirectURI: 'https://www.example.com/cb',
               state: 'xyz'
             }
           }
@@ -426,7 +491,7 @@ describe('ServerResponse#redirect', function() {
         req.session.state['123'] = {
           location: 'https://www.example.com/authorize/continue',
           clientID: 's6BhdRkqt3',
-          redirectURI: 'https://www.example.com/dashboard/cb',
+          redirectURI: 'https://www.example.com/cb',
           state: 'xyz'
         };
       })
@@ -454,7 +519,7 @@ describe('ServerResponse#redirect', function() {
             '123': {
               location: 'https://www.example.com/authorize/continue',
               clientID: 's6BhdRkqt3',
-              redirectURI: 'https://www.example.com/dashboard/cb',
+              redirectURI: 'https://www.example.com/cb',
               state: 'xyz'
             }
           }
@@ -468,8 +533,6 @@ describe('ServerResponse#redirect', function() {
       })
       .listen();
   }); // should redirect with current URL and state after saving modifications when processing a non-mutating request
-  
-  // TODO: same test case as above, but saving initial state
   
   it('should redirect with redirectURL and state to resume when successfully processing a mutating request', function(done) {
     var store = new SessionStore();
