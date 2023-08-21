15fe3ba380787c4d11b7ab7006e77fa8991a6b0f
Jared Hanson <jaredhanson@gmail.com>
2023-08-21 14:22:09 -0700

    Add tests.

--- test/request/pushstate.test.js
+++ test/request/pushstate.test.js
@@ -55,7 +55,7 @@ describe('IncomingMessage#pushState', function() {
       .listen();
   }); // should redirect after saving pushed state without state
   
-  it('should redirect after saving pushed state that redirects to referrer header', function(done) {
+  it('should redirect after saving pushed state that redirects to URL specified as referrer header', function(done) {
     var store = new SessionStore();
     sinon.spy(store, 'get');
     sinon.spy(store, 'set');
@@ -104,6 +104,57 @@ describe('IncomingMessage#pushState', function() {
         done();
       })
       .listen();
-  }); // should redirect after saving pushed state that redirects to referrer header
+  }); // should redirect after saving pushed state that redirects to URL specified as referrer header
+  
+  it('should redirect after saving pushed state that redirects to URL specified as query parameter', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+    
+    function handler(req, res, next) {
+      req.pushState({
+        provider: 'https://server.example.com'
+      }, 'https://www.example.com/cb');
+      res.redirect('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fwww.example.com%2Fcb');
+    }
+  
+    chai.express.use([ state({ store: store, genh: function() { return 'xyz' } }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/login/federated?return_to=https%3A%2F%2Fwww.example.com%2Fwelcome';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = { return_to: 'https://www.example.com/welcome' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fwww.example.com%2Fcb&state=xyz');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login/federated',
+          returnTo: 'https://www.example.com/welcome'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            'xyz': {
+              location: 'https://www.example.com/cb',
+              provider: 'https://server.example.com',
+              returnTo: 'https://www.example.com/welcome'
+            }
+          }
+        });
+        
+        expect(store.get).to.have.callCount(0);
+        expect(store.set).to.have.callCount(1);
+        expect(store.destroy).to.have.callCount(0);
+        
+        done();
+      })
+      .listen();
+  }); // should redirect after saving pushed state that redirects to URL specified as query parameter
   
 });
