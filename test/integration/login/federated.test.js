17786c06a7b491a4254c2818424dbb6ba2b0d309
Jared Hanson <jaredhanson@gmail.com>
2023-08-21 14:18:03 -0700

    Remove redundant test.

--- test/integration/login/federated.test.js
+++ test/integration/login/federated.test.js
@@ -7,62 +7,6 @@ var chai = require('chai')
 
 describe('GET /login/federated', function() {
   
-  it('should redirect with return location to pushed state', function(done) {
-    var store = new SessionStore();
-    sinon.spy(store, 'get');
-    sinon.spy(store, 'set');
-    sinon.spy(store, 'destroy');
-
-    function handler(req, res, next) {
-      expect(req.state).to.deep.equal({
-        location: 'https://client.example.com/login/federated',
-        returnTo: 'https://client.example.com/login'
-      });
-      
-      req.pushState({
-        provider: 'https://server.example.com'
-      }, 'https://client.example.com/cb');
-      res.redirect('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
-    }
-    
-    chai.express.use([ state({ store: store, genh: function() { return 'xyz' } }), handler ])
-      .request(function(req, res) {
-        req.connection = { encrypted: true };
-        req.method = 'GET';
-        req.url = '/login/federated?provider=https%3A%2F%2Fserver.example.com';
-        req.headers = {
-          'host': 'client.example.com',
-          'referer': 'https://client.example.com/login'
-        };
-        req.query = { provider: 'https://server.example.com' };
-        req.session = {};
-      })
-      .finish(function() {
-        expect(store.get).to.have.callCount(0);
-        expect(store.set).to.have.callCount(1);
-        expect(store.destroy).to.have.callCount(0);
-        
-        expect(this.req.state).to.deep.equal({
-          location: 'https://client.example.com/login/federated',
-          returnTo: 'https://client.example.com/login'
-        });
-        
-        expect(this.statusCode).to.equal(302);
-        expect(this.getHeader('Location')).to.equal('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb&state=xyz');
-        expect(this.req.session).to.deep.equal({
-          state: {
-            'xyz': {
-              location: 'https://client.example.com/cb',
-              provider: 'https://server.example.com',
-              returnTo: 'https://client.example.com/login'
-            }
-          }
-        });
-        done();
-      })
-      .listen();
-  }); // should redirect with state which then returns to referrer
-  
   it('should redirect with state which then returns to location', function(done) {
     var store = new SessionStore();
     sinon.spy(store, 'get');
