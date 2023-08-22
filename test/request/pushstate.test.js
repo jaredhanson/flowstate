2e7957e2d4d0cfdc2af01341d709dd5aa8c5a7f5
Jared Hanson <jaredhanson@gmail.com>
2023-08-22 10:14:55 -0700

    Add test.

--- test/request/pushstate.test.js
+++ test/request/pushstate.test.js
@@ -474,4 +474,59 @@ describe('IncomingMessage#pushState', function() {
       .listen();
   }); // should redirect within callback after saving pushed state with explicit handle that redirects to URL with state specified as query parameters
   
+  it('should external something', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+    
+    function handler(req, res, next) {
+      req.pushState({
+        clientID: req.query.client_id,
+        redirectURI: req.query.redirect_uri,
+        state: req.query.state
+      }, '/authorize/continue');
+      res.redirect('https://www.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fwww.example.com%2Fcb');
+    }
+  
+    chai.express.use([ state({ external: true, store: store, genh: function() { return 'xyz' } }), handler ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'GET';
+        req.url = '/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fwww%2Eexample%2Ecom%2Fcb';
+        req.headers = {
+          'host': 'www.example.com',
+          'referer': 'https://www.example.com/'
+        };
+        req.query = { response_type: 'code', client_id: 's6BhdRkqt3', state: 'xyz', redirect_uri: 'https://www.example.com/cb' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        // FIXME: this shouldn't have a return_to
+        //expect(this.getHeader('Location')).to.equal('https://www.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fwww.example.com%2Fcb&state=xyz');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/authorize',
+          returnTo: 'https://www.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fwww%2Eexample%2Ecom%2Fcb'
+        });
+        expect(this.req.session).to.deep.equal({
+          state: {
+            'xyz': {
+              location: 'https://www.example.com/authorize/continue',
+              clientID: 's6BhdRkqt3',
+              redirectURI: 'https://www.example.com/cb',
+              state: 'xyz'
+            }
+          }
+        });
+        
+        expect(store.get).to.have.callCount(0);
+        expect(store.set).to.have.callCount(1);
+        expect(store.destroy).to.have.callCount(0);
+        
+        done();
+      })
+      .listen();
+  }); // should external something
+  
 });
