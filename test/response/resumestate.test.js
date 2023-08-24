f89f5a071d0587789dd25f4581423e94bbbf1ff5
Jared Hanson <jaredhanson@gmail.com>
2023-08-24 15:55:34 -0700

    Clarify tests.

--- test/response/resumestate.test.js
+++ test/response/resumestate.test.js
@@ -49,6 +49,48 @@ describe('ServerResponse#resumeState', function() {
       .listen();
   }); // should proceed to next middleware when no state exists
   
+  it('should proceed to next middleware when no state exists due to ignoring invalid state parameter', function(done) {
+    var store = new SessionStore();
+    sinon.spy(store, 'get');
+    sinon.spy(store, 'set');
+    sinon.spy(store, 'destroy');
+  
+    function handler(req, res, next) {
+      res.resumeState(next);
+    }
+  
+    function home(req, res, next) {
+      res.redirect('/home');
+    }
+  
+    chai.express.use([ state({ store: store }), handler, home ])
+      .request(function(req, res) {
+        req.connection = { encrypted: true };
+        req.method = 'POST';
+        req.url = '/login';
+        req.headers = {
+          'host': 'www.example.com'
+        };
+        req.body = { state: 'xxx' };
+        req.session = {};
+      })
+      .finish(function() {
+        expect(this.statusCode).to.equal(302);
+        expect(this.getHeader('Location')).to.equal('/home');
+        expect(this.req.state).to.deep.equal({
+          location: 'https://www.example.com/login'
+        });
+        expect(this.req.session).to.deep.equal({});
+        
+        expect(store.get).to.have.callCount(1);
+        expect(store.set).to.have.callCount(0);
+        expect(store.destroy).to.have.callCount(0);
+        
+        done();
+      })
+      .listen();
+  }); // should proceed to next middleware when no state exists due to ignoring invalid state parameter
+  
   it('should redirect to URL specified as body parameter', function(done) {
     var store = new SessionStore();
     sinon.spy(store, 'get');
@@ -197,7 +239,7 @@ describe('ServerResponse#resumeState', function() {
     }
   
     function home(req, res, next) {
-      res.redirect('/home')
+      res.redirect('/home');
     }
   
     chai.express.use([ state({ store: store }), handler, home ])
@@ -249,7 +291,7 @@ describe('ServerResponse#resumeState', function() {
     }
   
     function home(req, res, next) {
-      res.redirect('/home')
+      res.redirect('/home');
     }
   
     chai.express.use([ state({ store: store }), handler, home ])
@@ -288,7 +330,7 @@ describe('ServerResponse#resumeState', function() {
       .listen();
   }); // should redirect with captured URL after completing current state when processing a mutating request
   
-  it('should yield arguments by encoding them as query parameters to redirect URL', function(done) {
+  it('should yield arguments by encoding them as query parameters of redirect URL', function(done) {
     var store = new SessionStore();
     sinon.spy(store, 'get');
     sinon.spy(store, 'set');
@@ -299,7 +341,7 @@ describe('ServerResponse#resumeState', function() {
     }
   
     function home(req, res, next) {
-      res.redirect('/home')
+      res.redirect('/home');
     }
   
     chai.express.use([ state({ store: store }), handler, home ])
@@ -346,48 +388,6 @@ describe('ServerResponse#resumeState', function() {
         done();
       })
       .listen();
-  }); // should yield arguments by encoding them as query parameters to redirect URL
-  
-  it('should ignore invalid state parameter', function(done) {
-    var store = new SessionStore();
-    sinon.spy(store, 'get');
-    sinon.spy(store, 'set');
-    sinon.spy(store, 'destroy');
-  
-    function handler(req, res, next) {
-      res.resumeState(next);
-    }
-  
-    function home(req, res, next) {
-      res.redirect('/home')
-    }
-  
-    chai.express.use([ state({ store: store }), handler, home ])
-      .request(function(req, res) {
-        req.connection = { encrypted: true };
-        req.method = 'POST';
-        req.url = '/login';
-        req.headers = {
-          'host': 'www.example.com'
-        };
-        req.body = { state: 'xxx' };
-        req.session = {};
-      })
-      .finish(function() {
-        expect(this.statusCode).to.equal(302);
-        expect(this.getHeader('Location')).to.equal('/home');
-        expect(this.req.state).to.deep.equal({
-          location: 'https://www.example.com/login'
-        });
-        expect(this.req.session).to.deep.equal({});
-        
-        expect(store.get).to.have.callCount(1);
-        expect(store.set).to.have.callCount(0);
-        expect(store.destroy).to.have.callCount(0);
-        
-        done();
-      })
-      .listen();
-  }); // should ignore invalid state parameter
+  }); // should yield arguments by encoding them as query parameters of redirect URL
   
 });
